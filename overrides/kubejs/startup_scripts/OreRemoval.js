aac394970d9cbb01b25e17b25eba1ca56c2f2796
Ghostipedia <Ghostipedia@gmail.com>
2023-12-05 21:19:12 -0500

    Establishing Harder Dimensional Gates
    Nether and End are now more difficult to access - Quests barely in as I had to remake them

--- overrides/kubejs/startup_scripts/OreRemoval.js
+++ overrides/kubejs/startup_scripts/OreRemoval.js
@@ -3,7 +3,16 @@ console.info('[COSMIC MANAGER] - TEST TEST 123 WORLDGEN FILE FOR ORE REMOVALS HI
 
 WorldgenEvents.remove(event => {
     event.removeOres(props => {
-      props.blocks = ['create:zinc_ore','create:deepslate_zinc_ore']
+      props.blocks = ['create:zinc_ore',
+      'create:deepslate_zinc_ore',
+      'forbidden_arcanus:deepslate_arcane_crystal_ore',
+      'forbidden_arcanus:arcane_crystal_ore',
+      'forbidden_arcanus:xpetrified_ore',
+      'forbidden_arcanus:stella_arcanum',
+      'forbidden_arcanus:runic_darkstone',
+      'forbidden_arcanus:runic_deepslate',
+      'forbidden_arcanus:runic_stone',
+      'occultism:silver_ore_deepslate',
+      'occultism:silver_ore']
     }) 
 })
-
