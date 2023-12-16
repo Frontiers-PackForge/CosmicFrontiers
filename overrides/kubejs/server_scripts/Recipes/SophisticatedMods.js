ea411b1a9f2af0d7fe208c1d57eccc96a81300a4
PurplikDev <vojta.g@atlas.cz>
2023-12-16 18:59:29 +0100

    soph storage done

--- overrides/kubejs/server_scripts/Recipes/SophisticatedMods.js
+++ overrides/kubejs/server_scripts/Recipes/SophisticatedMods.js
@@ -106,7 +106,7 @@ event.custom({
     }
   },
   'result': {
-    'item': 'sophisticatedbackpacks:diamond   _backpack' // Same case as the Registry name
+    'item': 'sophisticatedbackpacks:diamond_backpack' // Same case as the Registry name
   }
 })
 
@@ -122,7 +122,7 @@ event.custom({
 
   /* METHODS & UTILS */
 
-  const woodTypes = ['oak', 'spruce', 'birch', 'jungle', 'acacia', 'dark_oak', 'mangrove'];
+  const woodTypes = ['oak', 'spruce', 'birch', 'jungle', 'acacia', 'dark_oak', 'mangrove', 'crimson', 'warped'];
 
   let woodBarrel = (woodType) => {
 
@@ -140,13 +140,13 @@ event.custom({
               'item': 'minecraft:barrel'
           },
           'S': {
-            'item': 'minecraft:' + woodType + '_slab'
+            'item': `minecraft:${woodType}_slab`
           },
           'E': {
               'tag': 'forge:tools/saws'
           },
           'P': {
-              'item': 'minecraft:' + woodType + '_planks'
+              'item': `minecraft:${woodType}_planks`
           }
       },
       'pattern': [
@@ -156,24 +156,24 @@ event.custom({
       ],
       'result': {
           'item': 'sophisticatedstorage:barrel',
-          'nbt': '{woodType:\'' + woodType + '\'}'
+          'nbt': `{woodType:\'${woodType}\'}`
       }
   });
 }
 
-  let tierUpgrade = (materialOne, materialTwo, nextTier, previousTier) => {
+  let containerTierUpgrade = (materialOne, materialTwo, nextTier, previousTier) => {
       event.custom(
           {
               'type': 'sophisticatedstorage:storage_tier_upgrade',
               'conditions': [
                 {
                   'type': 'sophisticatedcore:item_enabled',
-                  'itemRegistryName': 'sophisticatedstorage:' + nextTier
+                  'itemRegistryName': `sophisticatedstorage:${nextTier}`
                 }
               ],
               'key': {
                   'C': {
-                      'item': 'sophisticatedstorage:' + previousTier
+                      'item': `sophisticatedstorage:${previousTier}`
                   },
                   'S': {
                       'tag': 'forge:tools/saws'
@@ -194,11 +194,34 @@ event.custom({
                   'PRP'
               ],
               'result': {
-                  'item': 'sophisticatedstorage:' + nextTier
+                  'item': `sophisticatedstorage:${nextTier}`
               }
           }
       );
   }
+
+  let upgradeUpgrade = (materialOne, materialTwo, coreItem, result) => {
+      event.shaped(result, [
+          'OTO', 
+          'TCT', 
+          'OTO'  
+        ], {
+          O: materialOne,  
+          T: materialTwo,
+          C: coreItem
+        })
+  }
+
+  let upgradeSmile = (input, result) => {
+      upgradeUpgrade(input, '#forge:dusts/redstone', 'sophisticatedstorage:upgrade_base', `sophisticatedstorage:${result}_upgrade`);
+      upgradeUpgrade('gtceu:rose_gold_screw', '#forge:dusts/redstone', `sophisticatedstorage:${result}_upgrade`, `sophisticatedstorage:advanced_${result}_upgrade`);
+}
+
+  let upgradeSmeltingStuff = (input, result) => {
+      upgradeUpgrade(input, '#forge:dusts/redstone', 'sophisticatedstorage:upgrade_base', `sophisticatedstorage:${result}_upgrade`);
+      upgradeUpgrade('gtceu:rose_gold_screw', '#forge:dusts/redstone', `sophisticatedstorage:${result}_upgrade`, `sophisticatedstorage:auto_${result}_upgrade`);
+  }
+
 
   /* STORAGE */
 
@@ -206,16 +229,50 @@ event.custom({
       woodBarrel(woodType);
   });
 
-  tierUpgrade('forge:rings/wrought_iron', 'gtceu:wrought_iron_plate', 'iron_barrel', 'barrel');
-  tierUpgrade('forge:rings/rose_gold', 'gtceu:rose_gold_plate', 'gold_barrel', 'iron_barrel');
-  tierUpgrade('forge:plates/diamond', 'gtceu:diamond_exquisite_gem' ,'diamond_barrel', 'gold_barrel');
+  containerTierUpgrade('forge:rings/wrought_iron', 'gtceu:wrought_iron_plate', 'iron_barrel', 'barrel');
+  containerTierUpgrade('forge:rings/rose_gold', 'gtceu:rose_gold_plate', 'gold_barrel', 'iron_barrel');
+  containerTierUpgrade('forge:plates/diamond', 'gtceu:diamond_exquisite_gem' ,'diamond_barrel', 'gold_barrel');
 
-  tierUpgrade('forge:rings/wrought_iron', 'gtceu:wrought_iron_plate', 'iron_chest', 'chest');
-  tierUpgrade('forge:rings/rose_gold', 'gtceu:rose_gold_plate', 'gold_chest', 'iron_chest');
-  tierUpgrade('forge:plates/diamond', 'gtceu:diamond_exquisite_gem' ,'diamond_chest', 'gold_chest');
+  containerTierUpgrade('forge:rings/wrought_iron', 'gtceu:wrought_iron_plate', 'iron_chest', 'chest');
+  containerTierUpgrade('forge:rings/rose_gold', 'gtceu:rose_gold_plate', 'gold_chest', 'iron_chest');
+  containerTierUpgrade('forge:plates/diamond', 'gtceu:diamond_exquisite_gem' ,'diamond_chest', 'gold_chest');
 
 
   /* UPGRADES */
+
+  upgradeUpgrade('gtceu:wood_screw', '#minecraft:planks', '#forge:tools/saws', 'sophisticatedstorage:upgrade_base');
+  upgradeUpgrade('gtceu:wood_screw', 'gtceu:wood_plate', 'sophisticatedstorage:upgrade_base', 'sophisticatedstorage:basic_tier_upgrade');
+  upgradeUpgrade('gtceu:wrought_iron_screw', 'gtceu:wood_plate', 'sophisticatedstorage:basic_tier_upgrade', 'sophisticatedstorage:basic_to_iron_tier_upgrade');
+  upgradeUpgrade('gtceu:rose_gold_screw', 'gtceu:wrought_iron_plate', '#forge:tools/screwdrivers', 'sophisticatedstorage:iron_to_gold_tier_upgrade');
+  upgradeUpgrade('gtceu:diamond_exquisite_gem', 'gtceu:rose_gold_plate', '#forge:tools/files', 'sophisticatedstorage:gold_to_diamond_tier_upgrade');
+
+
+
+  upgradeSmile('minecraft:sticky_piston', 'pickup');
+  upgradeSmile('#forge:string', 'filter');
+  upgradeSmile('#forge:ender_pearls', 'magnet');
+  upgradeSmile('#forge:golden_apple', 'feeding');
+  upgradeSmile('minecraft:piston', 'compacting');
+  upgradeSmile('#forge:obsidian', 'void');
+  upgradeSmeltingStuff('minecraft:furnace', 'smelting');
+  upgradeSmeltingStuff('minecraft:smoker', 'smoking');
+  upgradeSmeltingStuff('minecraft:blast_furnace', 'blasting');
+
+  upgradeSmile('minecraft:hopper', 'hopper');
+
+  upgradeUpgrade('minecraft:crafting_table', '#forge:dusts/redstone', 'sophisticatedstorage:upgrade_base', `sophisticatedstorage:crafting_upgrade`);
+  upgradeUpgrade('minecraft:stonecutter', '#forge:dusts/redstone', 'sophisticatedstorage:upgrade_base', `sophisticatedstorage:stonecutter_upgrade`);
+  upgradeUpgrade('minecraft:jukebox', '#forge:dusts/redstone', 'sophisticatedstorage:upgrade_base', `sophisticatedstorage:jukebox_upgrade`);
+  upgradeUpgrade('#forge:dusts/redstone', 'minecraft:piston', 'sophisticatedstorage:upgrade_base', `sophisticatedstorage:compression_upgrade`);
+
+
+// YES, I GAME UP HERE!
+// I'M FUCKING TIRED AHHHHHHHHHHHHHH
+
+  upgradeUpgrade('gtceu:wood_screw', 'gtceu:wood_plate', `sophisticatedstorage:upgrade_base`, `sophisticatedstorage:stack_upgrade_tier_1`);
+  upgradeUpgrade('gtceu:wrought_iron_screw', 'gtceu:wrought_iron_platee', `sophisticatedstorage:stack_upgrade_tier_1`, `sophisticatedstorage:stack_upgrade_tier_2`);
+  upgradeUpgrade('gtceu:rose_gold_screw', 'gtceu:rose_gold_plate', `sophisticatedstorage:stack_upgrade_tier_2`, `sophisticatedstorage:stack_upgrade_tier_3`);
+  upgradeUpgrade('gtceu:diamond_exquisite_gem', 'gtceu:diamond_plate', `sophisticatedstorage:stack_upgrade_tier_3`, `sophisticatedstorage:stack_upgrade_tier_4`);
 
   // will do later :3
 })
