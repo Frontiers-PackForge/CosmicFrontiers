8147c94238ec5b989f02256d028f62c5b112b005
Paniawesome <74797734+pani29@users.noreply.github.com>
2023-12-27 03:42:02 +1100

    Ad astra modifications (#35) - Yippie Merge Conflicts mk1
    * Added GT Integration to Ad Astra
    
    * Recipes for Fins, Engines & Nose Cones
    
    * Created a Item Generation File with the Ad Astra Stuff
    
    ---------
    
    Co-authored-by: Ghostipedia <46772882+Ghostipedia@users.noreply.github.com>

--- overrides/kubejs/server_scripts/Recipes/Gregtech.js
+++ overrides/kubejs/server_scripts/Recipes/Gregtech.js
@@ -29,20 +29,121 @@ ServerEvents.recipes(event => {
     event.shapeless('2x gtceu:fireclay_dust', [ 
            'gtceu:nether_brick_dust_dust',
            'gtceu:clay_dust'	
+      
+    event.recipes.gtceu.bender('kubejs:stainless_steel_fin') 
+         .itemInputs('6x gtceu:stainless_steel_plate')
+         .circuit(4)
+         .itemOutputs('kubejs:stainless_steel_fin')
+         .duration(100)
+         .EUt(480);
+    event.recipes.gtceu.assembler('kubejs:stainless_steel_nose_cone') 
+         .itemInputs('4x gtceu:stainless_steel_plate')
+         .itemInputs('gtceu:annealed_copper_rod')
+         .itemInputs('gtceu:annealed_copper_nugget')
+         .inputFluids('gtceu:polytetrafluoroethylene 288')
+         .itemOutputs('kubejs:stainless_steel_nose_cone')
+         .duration(100)
+         .EUt(480);  
+    event.recipes.gtceu.assembler('kubejs:stainless_steel_engine') 
+         .itemInputs('4x gtceu:stainless_steel_plate')
+         .itemInputs('gtceu:stainless_steel_rotor')
+         .itemInputs('gtceu:stainless_steel_drum')
+         .itemInputs('gtceu:hv_electric_motor')
+         .inputFluids('gtceu:polytetrafluoroethylene 432')
+         .itemOutputs('kubejs:stainless_steel_engine')
+         .duration(100)
+         .EUt(480);   
+    event.recipes.gtceu.bender('kubejs:hsse_fin') 
+         .itemInputs('6x gtceu:hsse_plate')
+         .circuit(4)
+         .itemOutputs('kubejs:hsse_fin')
+         .duration(100)
+         .EUt(4046);
+    event.recipes.gtceu.assembler('kubejs:hsse_nose_cone') 
+         .itemInputs('4x gtceu:hsse_plate')
+         .itemInputs('gtceu:annealed_copper_rod')
+         .itemInputs('gtceu:annealed_copper_nugget')
+         .inputFluids('gtceu:polytetrafluoroethylene 288')
+         .itemOutputs('kubejs:hsse_nose_cone')
+         .duration(100)
+         .EUt(4046);  
+    event.recipes.gtceu.assembler('kubejs:hsse_engine') 
+         .itemInputs('4x gtceu:hsse_plate')
+         .itemInputs('gtceu:stainless_steel_rotor')
+         .itemInputs('gtceu:iv_quantum_tank')
+         .itemInputs('gtceu:iv_electric_motor')
+         .inputFluids('gtceu:polytetrafluoroethylene 432')
+         .itemOutputs('kubejs:hsse_engine')
+         .duration(100)
+         .EUt(4046);
+    event.recipes.gtceu.bender('kubejs:osmiridium_fin') 
+         .itemInputs('6x gtceu:osmiridium_plate')
+         .circuit(4)
+         .itemOutputs('kubejs:osmiridium_fin')
+         .duration(100)
+         .EUt(30720);
+    event.recipes.gtceu.assembler('kubejs:osmiridium_nose_cone') 
+         .itemInputs('4x gtceu:osmiridium_plate')
+         .itemInputs('gtceu:annealed_copper_rod')
+         .itemInputs('gtceu:annealed_copper_nugget')
+         .inputFluids('gtceu:styrene_butadiene_rubber 288')
+         .itemOutputs('kubejs:osmiridium_nose_cone')
+         .duration(100)
+         .EUt(30720);  
+    event.recipes.gtceu.assembly_line('kubejs:osmiridium_engine') 
+         .itemInputs('4x gtceu:osmiridium_plate')
+         .itemInputs('2x gtceu:osmiridium_round')
+         .itemInputs('gtceu:stainless_steel_rotor')
+         .itemInputs('gtceu:niobium_titanium_small_fluid_pipe')
+         .itemInputs('gtceu:luv_quantum_tank')
+         .itemInputs('gtceu:luv_electric_motor')
+         .itemInputs('4x gtceu:hsss_screw')
+         .inputFluids('gtceu:styrene_butadiene_rubber 288')
+         .inputFluids('gtceu:soldering_alloy 432')
+         .itemOutputs('kubejs:osmiridium_engine')
+         .duration(100)
+         .EUt(30720);  
+         event.recipes.gtceu.bender('kubejs:americium_fin') 
+         .itemInputs('6x gtceu:americium_plate')
+         .circuit(4)
+         .itemOutputs('kubejs:americium_fin')
+         .duration(100)
+         .EUt(122800);
+    event.recipes.gtceu.assembler('kubejs:americium_nose_cone') 
+         .itemInputs('4x gtceu:americium_plate')
+         .itemInputs('2x gtceu:americium_round')
+         .itemInputs('gtceu:annealed_copper_rod')
+         .itemInputs('gtceu:annealed_copper_nugget')
+         .itemInputs('4x gtceu:osmiridium_screw')
+         .inputFluids('gtceu:styrene_butadiene_rubber 288')
+         .itemOutputs('kubejs:americium_nose_cone')
+         .duration(100)
+         .EUt(122800);  
+    event.recipes.gtceu.assembly_line('kubejs:americium_engine') 
+         .itemInputs('4x gtceu:americium_plate')
+         .itemInputs('gtceu:stainless_steel_rotor')
+         .itemInputs('gtceu:vanadium_gallium_single_cable')
+         .itemInputs('gtceu:zpm_quantum_tank')
+         .itemInputs('gtceu:zpm_electric_motor')
+         .itemInputs('4x gtceu:osmiridium_screw')
+         .itemInputs('4x gtceu:osmiridium_round')
+         .inputFluids('gtceu:styrene_butadiene_rubber 288')
+         .inputFluids('gtceu:soldering_alloy 432')
+         .itemOutputs('kubejs:americium_engine')
+         .duration(100)
+         .EUt(122800);  
+    event.recipes.gtceu.assembler('ad_astra:launch_pad') 
+         .itemInputs('4x gtceu:stainless_steel_plate')
+         .itemInputs('4x gtceu:steel_plate')
+         .itemOutputs('ad_astra:launch_pad')
+         .duration(100)
+         .EUt(480); 
+      
+      
+      
+      
          ])
-    event.recipes.gtceu.flora_nurturer('gtceu:nurture_lucis_spore') 
-         .itemInputs('betternether:lucis_spore')
-         .inputFluids(Fluid.of('minecraft:water', 1000))
-         .itemOutputs('cosmiccore:engourged_spore')
-         .itemOutputs('betternether:lucis_spore')
-         .chancedOutput(Item.of('betternether:lucis_spore', 1), 5000, 500)     
-         .duration(160)
-         .EUt(16); 
-    event.recipes.gtceu.nether_catalyzer('gtceu:catalyze_spore') 
-         .itemInputs('cosmiccore:engourged_spore')
-         .outputFluids(Fluid.of('gtceu:ethanol', 288))   
-         .duration(80)
-         .EUt(16); 
+ 
 
 //Crafting Recipes
 //Wrought Iron Base Molds
