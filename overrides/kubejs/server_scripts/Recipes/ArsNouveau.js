b66b00c17446101dd06a7553b1729bc690422c63
Ghostipedia <Ghostipedia@gmail.com>
2023-12-31 06:06:12 -0500

    Revert "0.2.0 Mega Commit bc Ghost has bad Github Practices"
    This reverts commit a2c2156ed9a3b5bec1893b3514a025d214c5367a.

--- overrides/kubejs/server_scripts/Recipes/ArsNouveau.js
+++ overrides/kubejs/server_scripts/Recipes/ArsNouveau.js
@@ -1,28 +1,28 @@
 ServerEvents.recipes(event => {
-    event.recipes.ars_nouveau.enchanting_apparatus(
+	event.recipes.ars_nouveau.enchanting_apparatus(
         [
             "minecraft:sand",
             "minecraft:sand",
             "minecraft:sand",
             "minecraft:sand",
         ], // input items
-        "minecraft:gunpowder", // reagent
-        "minecraft:tnt", // output
-        1000, // source cost
-        // true // keep nbt of reagent, think like a smithing recipe
-    );
+	    "minecraft:gunpowder", // reagent
+	    "minecraft:tnt", // output
+	    1000, // source cost
+	    // true // keep nbt of reagent, think like a smithing recipe
+	);
 
 })
 
 //Time to do some Gregification
 //Spellbooks
 ServerEvents.recipes(e => {
-    e.remove({ id: 'ars_nouveau:novice_spell_book' })
+    e.remove({id: 'ars_nouveau:novice_spell_book'})
     e.shaped('ars_nouveau:novice_spell_book', [
-        'ABC',
-        'DE ',
-        '   '
-    ],
+            'ABC',
+            'DE ',
+            '   '
+        ],
         {
             A: 'minecraft:book',
             B: 'gtceu:iron_shovel',
@@ -60,16 +60,16 @@ ServerEvents.recipes(e => {
     //    }
     //)
 
-    //Fiber Recipes
-    e.remove({ id: 'ars_nouveau:magebloom_fiber' })
+//Fiber Recipes
+    e.remove({id: 'ars_nouveau:magebloom_fiber'})
     e.recipes.gtceu.spooling_machine('ars_nouveau:magebloom_fiber')
         .itemInputs('ars_nouveau:magebloom')
         .itemOutputs('ars_nouveau:magebloom_fiber')
         .duration(60)
         .EUt(4);
-    //Spooling Recipes
+//Spooling Recipes
 
-    e.remove({ output: 'ars_nouveau:blank_thread' })
+    e.remove({output: 'ars_nouveau:blank_thread'})
     e.recipes.gtceu.spooling_machine('ars_nouveau:blank_thread')
         .itemInputs('4x ars_nouveau:magebloom_fiber')
         .itemInputs('gtceu:gold_rod')
@@ -77,13 +77,13 @@ ServerEvents.recipes(e => {
         .duration(60)
         .EUt(4);
 
-    //Magic Stations
-    e.remove({ id: 'ars_nouveau:arcane_core' })
+//Magic Stations
+    e.remove({id: 'ars_nouveau:arcane_core'})
     e.shaped('ars_nouveau:arcane_core', [
-        'ACA',
-        'BDB',
-        'AAA'
-    ],
+            'ACA',
+            'BDB',
+            'AAA'
+        ],
         {
             A: 'ars_nouveau:sourcestone',
             B: 'gtceu:gold_rod',
@@ -91,12 +91,12 @@ ServerEvents.recipes(e => {
             D: 'ars_nouveau:source_gem',
         }
     )
-    e.remove({ id: 'ars_nouveau:enchanting_apparatus' })
+    e.remove({id: 'ars_nouveau:enchanting_apparatus'})
     e.shaped('ars_nouveau:enchanting_apparatus', [
-        'BAB',
-        'CDC',
-        'BAB'
-    ],
+            'BAB',
+            'CDC',
+            'BAB'
+        ],
         {
             A: 'ars_nouveau:sourcestone',
             B: 'minecraft:gold_nugget',
@@ -104,49 +104,39 @@ ServerEvents.recipes(e => {
             D: 'minecraft:diamond',
         }
     )
-    e.remove({ id: 'ars_nouveau:agronomic_sourcelink' })
+    e.remove({id: 'ars_nouveau:agronomic_sourcelink'})
     e.shaped('ars_nouveau:agronomic_sourcelink', [
-        ' A ',
-        'BCB',
-        ' A '
-    ],
+            ' A ',
+            'BCB',
+            ' A '
+        ],
         {
             A: 'ars_nouveau:source_gem',
             B: 'gtceu:gold_plate',
             C: 'minecraft:wheat',
         }
     )
-    e.remove({ id: 'ars_nouveau:source_jar' })
+    e.remove({id: 'ars_nouveau:source_jar'})
     e.shaped('ars_nouveau:source_jar', [
-        'CBC',
-        'A A',
-        'CCC'
-    ],
+            'CBC',
+            'A A',
+            'CCC'
+        ],
         {
             A: '#forge:glass',
             B: 'gtceu:gold_plate',
             C: 'ars_nouveau:archwood_slab',
         }
     )
-    e.remove({ id: 'ars_nouveau:relay' })
+    e.remove({id: 'ars_nouveau:relay'})
     e.shaped('ars_nouveau:relay', [
-        'A A',
-        'ABA',
-        'A A'
-    ],
+            'A A',
+            'ABA',
+            'A A'
+        ],
         {
             A: 'gtceu:gold_rod',
             B: 'ars_nouveau:source_gem_block',
         }
-    )
-    e.remove({ output: 'ars_nouveau:imbuement_chamber' })
-    e.shaped('ars_nouveau:imbuement_chamber', [
-      'ARA',
-      'A A',
-      'ARA'
-    ], {
-      A: 'ars_nouveau:archwood_planks',
-      R: 'gtceu:rose_gold_plate'
-    }
     )
 })
--- overrides/kubejs/startup_scripts/Materials/MaterialGen.js
+++ overrides/kubejs/startup_scripts/Materials/MaterialGen.js
@@ -1,38 +1,23 @@
-
-GTCEuStartupEvents.registry('gtceu:material_icon_set', (event) => {
-    event.create('mana').parent(GTMaterialIconSet.DULL)
-})
-
 GTCEuStartupEvents.registry('gtceu:material', event => {
     event.create('nether_brick_dust')
         .dust() 
         .color(0x511515)
-    event.create('zanite')
+        event.create('zanite')
         .gem()
         .ore()
         .color(0xa521fc).iconSet(GTMaterialIconSet.DIAMOND)
-    event.create('nether_sediment_sludge')
-        .fluid()
-        .color(0x511515)
-    event.create('mana_steel')
-        // .ingot()
-        .flags(GTMaterialFlags.GENERATE_FINE_WIRE, GTMaterialFlags.GENERATE_PLATE, GTMaterialFlags.GENERATE_ROD)
-        .fluid()
-        .color(0x67b9ee)
-        .cableProperties(GTValues.V[GTValues.LV],8,0,false)
-        .iconSet(GTMaterialIconSet.getByName('mana'))
-//nether materials, no ore veins yet 
+//nether materials, no ore veins yet
     event.create('cincinnasite')
-        .ingot()
-        .ore()
-        .color(0xeebb63).iconSet(GTMaterialIconSet.SHINY)
-        .flags(GTMaterialFlags.GENERATE_PLATE, GTMaterialFlags.GENERATE_ROD)
-        
+    .ingot()
+    .ore()
+    .color(0xeebb63).iconSet(GTMaterialIconSet.SHINY)
+    .flags(GTMaterialFlags.GENERATE_PLATE, GTMaterialFlags.GENERATE_ROD)
+    
 
     event.create('nether_ruby')
-        .gem()
-        .ore()
-        .flags(GTMaterialFlags.GENERATE_PLATE, GTMaterialFlags.GENERATE_ROD)
-        .color(0x9f0b21).iconSet(GTMaterialIconSet.RUBY)
+    .gem()
+    .ore()
+    .flags(GTMaterialFlags.GENERATE_PLATE, GTMaterialFlags.GENERATE_ROD)
+    .color(0x9f0b21).iconSet(GTMaterialIconSet.RUBY)
 
 })
