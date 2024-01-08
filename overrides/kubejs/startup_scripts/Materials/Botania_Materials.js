70eb3e6855e0ff16795c198647e763533b4cb05b
Srdra <99605300+srdr2k3@users.noreply.github.com>
2024-01-08 13:37:03 +0100

    Materials and Lang

---
+++ overrides/kubejs/startup_scripts/Materials/Botania_Materials.js
@@ -0,0 +1,19 @@
+//Materials
+GTCEuStartupEvents.registry('gtceu:material', event => {
+    event.create('livingrock')
+    .color(0xd7d7c8)
+    .iconSet(GTMaterialIconSet.ROUGH)
+    .flags(
+        GTMaterialFlags.GENERATE_PLATE
+    )
+
+    event.create('livingwood')
+        .color(0x34140c)
+        .iconSet(GTMaterialIconSet.WOOD)
+        .flags(
+            GTMaterialFlags.GENERATE_PLATE,
+            GTMaterialFlags.GENERATE_BOLT_SCREW,
+            GTMaterialFlags.GENERATE_FRAME
+        )
+
+    })
