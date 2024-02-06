40bc8abe07c4a633c0fb3b001b2a3e09ceb14b91
Ghostipedia <Ghostipedia@gmail.com>
2024-02-06 13:11:56 -0500

    WTF Titanium Chain patches
    Never in my life have I questioned a PR more

--- overrides/kubejs/server_scripts/Recipes/Chains/Bauxite.js
+++ overrides/kubejs/server_scripts/Recipes/Chains/Bauxite.js
@@ -4,6 +4,7 @@ ServerEvents.recipes(event => {
     event.remove({ id: 'gtceu:thermal_centrifuge/centrifuge_bauxite_crushed_ore_to_refined_ore' })
     event.remove({ id: 'gtceu:macerator/macerate_bauxite_crushed_ore_to_dust' })
     event.remove({ id: 'gtceu:centrifuge/centrifuge_bauxite_pure_dust_to_dust' })
+    event.remove({ id: 'gtceu:extractor/extract_cryolite_dust' })
 
     event.recipes.gtceu.thermal_centrifuge('centrifuge_bauxite_purified_ore_to_refined_ore')
         .itemInputs('gtceu:purified_bauxite_ore')
@@ -53,27 +54,32 @@ ServerEvents.recipes(event => {
     //     .duration(400)
     //     .EUt(GTValues.VA[GTValues.MV]);
 
-
+    event.recipes.gtceu.mixer('sodium_hydroxide_solution')
+    .itemInputs('gtceu:sodium_hydroxide_dust')
+    .inputFluids('minecraft:water 1000')
+    .outputFluids('gtceu:sodium_hydroxide_solution 1000')
+    .duration(20)
+    .EUt(GTValues.VA[GTValues.LV]);
 
     event.recipes.gtceu.mixer('gtceu:mixer/sodium_hydroxide_bauxite_from_bauxite')
-        .itemInputs('29x gtceu:bauxite_dust')
-        .inputFluids('gtceu:sodium_hydroxide_solution 24000')
-        .outputFluids('gtceu:sodium_hydroxide_bauxite 24000')
-        .duration(240)
+        .itemInputs('29x gtceu:small_bauxite_dust')
+        .inputFluids('gtceu:sodium_hydroxide_solution 6000')
+        .outputFluids('gtceu:sodium_hydroxide_bauxite 6000')
+        .duration(60)
         .EUt(GTValues.VA[GTValues.LV]);
 
     event.recipes.gtceu.fluid_heater('gtceu:fluid_heater/sodium_hydroxide_bauxite_to_heated')
         .circuit(0)
-        .inputFluids('gtceu:sodium_hydroxide_bauxite 24000')
-        .outputFluids('gtceu:heated_sodium_hydroxide_bauxite 24000')
+        .inputFluids('gtceu:sodium_hydroxide_bauxite 6000')
+        .outputFluids('gtceu:heated_sodium_hydroxide_bauxite 6000')
         .duration(30)
         .EUt(GTValues.VA[GTValues.LV]);
 
-    event.recipes.gtceu.chemical_reactor('gtceu:chemical_reactor/bauxite_to_red_mud')
+    event.recipes.gtceu.centrifuge('bauxite_to_red_mud')
         .circuit(0)
-        .inputFluids('gtceu:heated_sodium_hydroxide_bauxite 24000')
-        .outputFluids('gtceu:red_mud 6000')
-        .outputFluids('gtceu:sodium_aluminate_solution 18000')
+        .inputFluids('gtceu:heated_sodium_hydroxide_bauxite 12000')
+        .outputFluids('gtceu:red_mud 3000')
+        .outputFluids('gtceu:sodium_aluminate_solution 9000')
         .duration(230)
         .EUt(GTValues.VA[GTValues.MV]);
 
@@ -82,31 +88,31 @@ ServerEvents.recipes(event => {
         .inputFluids('gtceu:sodium_aluminate_solution 9000')
         .itemOutputs('32x gtceu:sodium_aluminate_dust')
         .itemOutputs('3x gtceu:sodium_silicate_dust')
-        .outputFluids('gtceu:gallium_liquor 50')
+        .chancedOutput(Item.of('gtceu:gallium_dust', 1), 1000, 500)
         .duration(120)
         .EUt(GTValues.VA[GTValues.HV]);
 
-    event.recipes.gtceu.chemical_reactor('gtceu:chemical_reactor/sodium_amalgam')
-        .itemInputs('3x gtceu:sodium_dust')
-        .inputFluids('gtceu:mercury 1000')
-        .itemOutputs('4x gtceu:sodium_amalgam_dust')
-        .duration(200)
-        .EUt(GTValues.VA[GTValues.MV]);
+    // event.recipes.gtceu.chemical_reactor('gtceu:chemical_reactor/sodium_amalgam')
+    //     .itemInputs('3x gtceu:sodium_dust')
+    //     .inputFluids('gtceu:mercury 1000')
+    //     .itemOutputs('4x gtceu:sodium_amalgam_dust')
+    //     .duration(200)
+    //     .EUt(GTValues.VA[GTValues.MV]);
 
-    event.recipes.gtceu.chemical_reactor('gtceu:chemical_reactor/gallium_amalgam')
-        .itemInputs('4x gtceu:sodium_amalgam_dust')
-        .inputFluids('gtceu:gallium_liquor 1000')
-        .itemOutputs('9x gtceu:sodium_hydroxide_dust')
-        .itemOutputs('2x gtceu:gallium_amalgam_dust')
-        .duration(200)
-        .EUt(GTValues.VA[GTValues.MV]);
+    // event.recipes.gtceu.chemical_reactor('gtceu:chemical_reactor/gallium_amalgam')
+    //     .itemInputs('4x gtceu:sodium_amalgam_dust')
+    //     .inputFluids('gtceu:gallium_liquor 1000')
+    //     .itemOutputs('9x gtceu:sodium_hydroxide_dust')
+    //     .itemOutputs('2x gtceu:gallium_amalgam_dust')
+    //     .duration(200)
+    //     .EUt(GTValues.VA[GTValues.MV]);
 
-    event.recipes.gtceu.electrolyzer('gtceu:electrolyzer/gallium_amalgam_decomp')
-        .itemInputs('2x gtceu:gallium_amalgam_dust')
-        .itemOutputs('1x gtceu:gallium_dust')
-        .outputFluids('gtceu:mercury 1000')
-        .duration(200)
-        .EUt(GTValues.VA[GTValues.MV]);
+    // event.recipes.gtceu.electrolyzer('gtceu:electrolyzer/gallium_amalgam_decomp')
+    //     .itemInputs('2x gtceu:gallium_amalgam_dust')
+    //     .itemOutputs('1x gtceu:gallium_dust')
+    //     .outputFluids('gtceu:mercury 1000')
+    //     .duration(200)
+    //     .EUt(GTValues.VA[GTValues.MV]);
 
     event.recipes.gtceu.chemical_reactor('gtceu:chemical_reactor/aluminium_hydroxide_bad')
         .itemInputs('8x gtceu:sodium_aluminate_dust')
@@ -129,7 +135,7 @@ ServerEvents.recipes(event => {
     event.recipes.gtceu.electric_blast_furnace('gtceu:electric_blast_furnace/alumina')
         .itemInputs('14x gtceu:aluminium_hydroxide_dust')
         .itemOutputs('5x gtceu:alumina_dust')
-        .outputFluids('gtceu:water 3000')
+        .outputFluids('minecraft:water 3000')
         .blastFurnaceTemp(1100)
         .duration(200)
         .EUt(GTValues.VA[GTValues.MV]);
@@ -168,37 +174,37 @@ ServerEvents.recipes(event => {
         .duration(640)
         .EUt(GTValues.VA[GTValues.MV]);
 
-    event.recipes.gtceu.chemical_reactor('gtceu:chemical_reactor/cryolite_synth')
-        .itemInputs('18x gtceu:sodium_hydroxide_dust')
-        .itemInputs('5x gtceu:alumina_dust')
-        .inputFluids('gtceu:hydrofluoric_acid 12000')
-        .itemOutputs('20x gtceu:cryolite_dust')
-        .outputFluids('gtceu:water 9000')
-        .duration(400)
-        .EUt(GTValues.VA[GTValues.MV]);
+    // event.recipes.gtceu.chemical_reactor('gtceu:chemical_reactor/cryolite_synth')
+    //     .itemInputs('18x gtceu:sodium_hydroxide_dust')
+    //     .itemInputs('5x gtceu:alumina_dust')
+    //     .inputFluids('gtceu:hydrofluoric_acid 12000')
+    //     .itemOutputs('20x gtceu:cryolite_dust')
+    //     .outputFluids('gtceu:water 9000')
+    //     .duration(400)
+    //     .EUt(GTValues.VA[GTValues.MV]);
 
-    event.recipes.gtceu.electrolyzer('gtceu:electrolyzer/aluminium_dust_from_alumina')
-        .itemInputs('10x gtceu:alumina_dust')
-        .inputFluids('gtceu:cryolite 1000')
-        .inputFluids('gtceu:oxygen 6000')
-        .itemOutputs('4x gtceu:aluminium_dust')
-        .itemOutputs('6x gtceu:sodium_fluoride_dust')
-        .itemOutputs('4x gtceu:aluminium_trifluoride_dust')
-        .inputFluids('gtceu:water 9000')
-        .duration(200)
-        .EUt(GTValues.VA[GTValues.HV]);
+    // event.recipes.gtceu.electrolyzer('gtceu:electrolyzer/aluminium_dust_from_alumina')
+    //     .itemInputs('10x gtceu:alumina_dust')
+    //     .inputFluids('gtceu:cryolite 1000')
+    //     .inputFluids('gtceu:oxygen 6000')
+    //     .itemOutputs('4x gtceu:aluminium_dust')
+    //     .itemOutputs('6x gtceu:sodium_fluoride_dust')
+    //     .itemOutputs('4x gtceu:aluminium_trifluoride_dust')
+    //     .inputFluids('gtceu:water 9000')
+    //     .duration(200)
+    //     .EUt(GTValues.VA[GTValues.HV]);
 
-    event.recipes.gtceu.chemical_reactor('gtceu:chemical_reactor/cryolite_main')
-        .itemInputs('6x gtceu:sodium_fluoride_dust')
-        .itemInputs('4x gtceu:aluminium_trifluoride_dust')
-        .itemOutputs('4x gtceu:cryolite_dust')
-        .duration(200)
-        .EUt(GTValues.VA[GTValues.MV]);
+    // event.recipes.gtceu.chemical_reactor('gtceu:chemical_reactor/cryolite_main')
+    //     .itemInputs('6x gtceu:sodium_fluoride_dust')
+    //     .itemInputs('4x gtceu:aluminium_trifluoride_dust')
+    //     .itemOutputs('4x gtceu:cryolite_dust')
+    //     .duration(200)
+    //     .EUt(GTValues.VA[GTValues.MV]);
 
-    event.recipes.gtceu.fluid_heater('gtceu:fluid_heater/liquid_cryolite')
-        .circuit(1)
-        .itemInputs('1x gtceu:cryolite_dust')
-        .outputFluids('gtceu:cryolite 100')
-        .duration(30)
-        .EUt(GTValues.VA[GTValues.LV]);
+    // event.recipes.gtceu.chemical_reactor('liquid_cryolite')
+    //     .circuit(1)
+    //     .itemInputs('1x gtceu:cryolite_dust')
+    //     .outputFluids('gtceu:cryolite 100')
+    //     .duration(30)
+    //     .EUt(GTValues.VA[GTValues.LV]);
 })
