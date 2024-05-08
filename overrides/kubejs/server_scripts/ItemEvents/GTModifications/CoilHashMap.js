20dd221ed2371056d23bac2caf9f1faf4d517026
Ghostipedia <Ghostipedia@gmail.com>
2024-05-08 06:57:59 -0400

    Land + Misc Testing Machine

---
+++ overrides/kubejs/server_scripts/ItemEvents/GTModifications/CoilHashMap.js
@@ -0,0 +1,8 @@
+// const $GTBlocks = Java.loadClass('com.gregtechceu.gtceu.common.data.GTBlocks')
+
+// ServerEvents.loaded(event => {
+//     $GTBlocks.ALL_COILS.remove(CoilBlock.CoilType.RTMALLOY)
+// })
+
+
+//Return to this at a later date.
