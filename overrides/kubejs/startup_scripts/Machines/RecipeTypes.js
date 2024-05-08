bfe3aac828ddcaedbd1b046571f1e0ce13bcc970
Ghostipedia <Ghostipedia@gmail.com>
2024-05-08 06:57:26 -0400

    Update Mixer to Have 3 Fluid Slots

--- overrides/kubejs/startup_scripts/Machines/RecipeTypes.js
+++ overrides/kubejs/startup_scripts/Machines/RecipeTypes.js
@@ -114,4 +114,7 @@ GTCEuStartupEvents.registry('gtceu:recipe_type', event => {
         .setSlotOverlay(false, false, GuiTextures.SOLIDIFIER_OVERLAY)
         .setProgressBar(GuiTextures.PROGRESS_BAR_ARROW, FillDirection.LEFT_TO_RIGHT)
         .setSound(GTSoundEntries.TURBINE)
+
+    GTRecipeTypes.MIXER_RECIPES.setMaxIOSize(6, 1, 3, 1)
+
 })
