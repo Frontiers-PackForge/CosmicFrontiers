918069e795fac3e0149013a8c6779141edf0545c
Ghostipedia <Ghostipedia@gmail.com>
2024-05-08 06:54:10 -0400

    Mars OreGen Work

---
+++ overrides/kubejs/startup_scripts/WorldGen/WorldGenLayer/MarsWorldGenLayer.js
@@ -0,0 +1,6 @@
+//ad_astra:moon
+GTCEuStartupEvents.registry('gtceu:world_gen_layer', event => {
+    event.create('mars_stone')
+        .targets('ad_astra:mars_stone')
+        .dimensions('ad_astra:mars') 
+})
