d8e47097912684456f827dffcea2b1a4dcc3670d
MrQuentin <32539717+mrquentin@users.noreply.github.com>
2024-08-17 11:56:18 -0400

    Farmer's Delight - Part I (Signs)

--- overrides/kubejs/server_scripts/Recipes/Foods/Tags.js
+++ overrides/kubejs/server_scripts/Recipes/Foods/Tags.js
@@ -1,6 +1,10 @@
 ServerEvents.tags('item', event => {
     event.add('forge:flour', 'gtceu:wheat_dust')
     event.add('forge:salt', 'gtceu:salt_dust')
+    event.add('forge:rice', 'farmersdelight:rice')
+    event.add('forge:cabbage', 'farmersdelight:cabbage')
+    event.add('forge:tomatoes', 'farmersdelight:tomato')
+    event.add('forge:onions', 'farmersdelight:onion')
 })
 
 ServerEvents.tags('fluid', event => {
