c0cf6d157ddb691af6608efdd0219340d43381a6
Karthi Suresh <75553966+JuiceyBeans@users.noreply.github.com>
2024-08-23 22:31:50 +0400

    Oops add drills PROPERLY this time

--- overrides/kubejs/server_scripts/ItemEvents/Ultimine.js
+++ overrides/kubejs/server_scripts/ItemEvents/Ultimine.js
@@ -2,19 +2,26 @@
 ServerEvents.tags('item', event => {
   //Exclude Hammers - Removing this causes some extreme issues with ultimine - You have been warned.
   GTMaterialRegistry.getRegisteredMaterials().forEach(id => {
-    event.add('ftbultimine:excluded_tools', [`gtceu:${id.name}_mining_hammer`, `gtceu:${id.name}_drill`])
+    event.add('ftbultimine:excluded_tools', [
+      `gtceu:${id.name}_mining_hammer`, 
+      `gtceu:lv_${id.name}_drill`,
+      `gtceu:mv_${id.name}_drill`,
+      `gtceu:hv_${id.name}_drill`,
+      `gtceu:ev_${id.name}_drill`,
+      `gtceu:iv_${id.name}_drill`,      
+    ])
   })
 })
 //This removes GTCEU Ores from Ultimine - This is a balance change so removing/chaning this is ill advised. I won't invalid.nameate your run but please bear in mind it's either this or I remove Ultimine entirely. 
 ServerEvents.tags('block', event => {
   GTMaterialRegistry.getRegisteredMaterials().forEach(id => {
     event.add('ftbultimine:excluded_blocks', [
-      `gtceu:andesite_${id.name}_ore`, 
-      `gtceu:diorite_${id.name}_ore`, 
-      `gtceu:deepslate_${id.name}_ore`, 
-      `gtceu:${id.name}_ore`, 
-      `gtceu:granite_${id.name}_ore`, 
-      `gtceu:netherrack_${id.name}_ore`, 
+      `gtceu:${id.name}_ore`,
+      `gtceu:andesite_${id.name}_ore`,
+      `gtceu:diorite_${id.name}_ore`,
+      `gtceu:deepslate_${id.name}_ore`,
+      `gtceu:granite_${id.name}_ore`,
+      `gtceu:netherrack_${id.name}_ore`,
       `gtceu:endstone_${id.name}_ore`,
       `gtceu:depthrock_${id.name}_ore`,
       `gtceu:shiverstone_${id.name}_ore`,
