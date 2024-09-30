7cb704ca78a876e4b7cf8b6d35dc71a1cc65e07f
pinger80 <17233001+pinger80@users.noreply.github.com>
2024-09-30 05:10:37 +0300

    feral flare lantern

---
+++ overrides/kubejs/server_scripts/Torchmaster.js
@@ -0,0 +1,12 @@
+ServerEvents.recipes(event => {
+	event.remove({ id: 'torchmaster:feral_flare_lantern' })
+	event.shaped( 'torchmaster:feral_flare_lantern', [
+		' B ',
+		'PGP',
+		' B '
+	], {
+		B: 'gtceu:brass_plate'
+		P: 'gtceu:glass_plate'
+		G: 'minecraft:glowstone'
+	})
+})
