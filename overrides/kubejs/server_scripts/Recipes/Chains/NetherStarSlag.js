61381ce91315658f2a25494138960941b24f6954
WamenNudles <jackmunsell@yahoo.com>
2024-09-29 22:47:39 -0400

    fixed dimension name
    wrong dimension tag fixed

--- overrides/kubejs/server_scripts/Recipes/Chains/NetherStarSlag.js
+++ overrides/kubejs/server_scripts/Recipes/Chains/NetherStarSlag.js
@@ -8,7 +8,7 @@ ServerEvents.recipes(event => {
         .itemInputs('64x gtceu:silicon_dioxide_dust')
         .inputFluids('gtceu:deeply_withered_nether_slag 16000')
         .outputFluids('gtceu:silica_enriched_nether_slag 8000')
-        .dimension('minecraft:nether')
+        .dimension('minecraft:the_nether')
         .duration(400)
         .EUt(GTValues.VA[GTValues.EV]);
     event.recipes.gtceu.distillation_tower('silica_slag_distillation')
@@ -16,14 +16,14 @@ ServerEvents.recipes(event => {
         .outputFluids('gtceu:nether_star_distillate_slurry 1000')
         .outputFluids('gtceu:deeply_withered_nether_slag 2000')
         .outputFluids('gtceu:pyroflux 1000')
-        .dimension('minecraft:nether')
+        .dimension('minecraft:the_nether')
         .duration(100)
         .EUt(GTValues.VA[GTValues.IV]);
     event.recipes.gtceu.chemical_dehydrator('slurry_drying')
         .itemInputs('gtceu:molybdenum_dust')
         .inputFluids('gtceu:nether_star_distillate_slurry 1000')
         .itemOutputs('4x gtceu:dull_nether_star_dust')
-        .dimension('minecraft:nether')
+        .dimension('minecraft:the_nether')
         .duration(200)
         .EUt(GTValues.VA[GTValues.EV]);
     event.recipes.gtceu.electric_blast_furnace('star_boule')
@@ -31,13 +31,13 @@ ServerEvents.recipes(event => {
         .inputFluids('cosmiccore:prisma 4000')
         .itemOutputs('kubejs:chromatically_doped_nether_star_boule')
         .blastFurnaceTemp(4500)
-        .dimension('minecraft:nether')
+        .dimension('minecraft:the_nether')
         .duration(900)
         .EUt(GTValues.VA[GTValues.EV]);
     event.recipes.gtceu.forge_hammer('shatter_the_boule')
         .itemInputs('kubejs:chromatically_doped_nether_star_boule')
-        .itemOutputs(['2x minecraft:nether_star', 'gtceu:nether_star_dust', '2x gtceu:dull_nether_star_dust'])
-        .dimension('minecraft:nether')
+        .itemOutputs(['2x minecraft:nether_star'/*, 'gtceu:nether_star_dust', '2x gtceu:dull_nether_star_dust'*/])
+        .dimension('minecraft:the_nether')
         .duration(150)
         .EUt(GTValues.VA[GTValues.EV]);
 
