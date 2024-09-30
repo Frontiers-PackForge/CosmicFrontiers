5597e21e460ef842632afa3e077a35a557525aec
pinger80 <17233001+pinger80@users.noreply.github.com>
2024-09-30 05:23:11 +0300

    Update Torchmaster.js
    megatorch, dread lamp

--- overrides/kubejs/server_scripts/Torchmaster.js
+++ overrides/kubejs/server_scripts/Torchmaster.js
@@ -7,6 +7,34 @@ ServerEvents.recipes(event => {
 	], {
 		B: 'gtceu:brass_plate',
 		P: 'gtceu:glass_plate',
-		G: 'minecraft:glowstone'
+		G: 'minecraft:glowstone_dust'
+	})
+	
+	event.remove({ id: 'torchmaster:megatorch' })
+	event.shaped( 'torchmaster:megatorch', [
+		'RCR',
+		'MLS',
+		'GDG'
+	], {
+		R: 'gtceu:gold_ring',
+		C: 'gtceu:coke_block',
+		M: 'forge:tools/mallets',
+		L: 'minecraft:logs',
+		S: 'forge:tools/saws',
+		G: 'minecraft:gold_block',
+		D: 'minecraft:diamond'
+	})
+	
+	event.remove({ id: 'torchmaster:dreadlamp' })
+	event.shaped( 'torchmaster:dreadlamp', [
+		'RSR',
+		'PGP',
+		'RHR'
+	], {
+		P: 'gtceu:glass_plate',
+		G: 'minecraft:glowstone',
+		R: 'gtceu:steel_rod',
+		S: 'gtceu:steel_plate',
+		H: 'forge:tools/hammers'
 	})
 })
