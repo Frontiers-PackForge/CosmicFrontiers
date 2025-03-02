fec8829c54ffe9340ae690163d3104d737267565
Karthi Suresh <75553966+JuiceyBeans@users.noreply.github.com>
2025-03-02 12:24:40 +0000

    Move Psi and Mana diet groups to Kube tags

--- overrides/kubejs/server_scripts/Recipes/Foods/Tags.js
+++ overrides/kubejs/server_scripts/Recipes/Foods/Tags.js
@@ -5,16 +5,32 @@ ServerEvents.tags('item', event => {
     event.add('forge:cabbage', 'farmersdelight:cabbage')
     event.add('forge:tomatoes', 'farmersdelight:tomato')
     event.add('forge:onions', 'farmersdelight:onion')
+    
+    proteins('nethersdelight:hoglin_loin')
+    mana('ars_nouveau:source_berry_roll')
+    mana('ars_nouveau:source_berry_pie')
+    mana('ars_nouveau:sourceberry_bush')
+    psi('gtceu:basic_electronic_circuit')
+
+    // Dietary groups
+    function mana(item) {
+        event.add('diet:mana', item)
+    }
+    function psi(item) {
+        event.add('diet:psi', item)
+    }
+    function proteins(item) {
+        event.add('diet:proteins', item)
+    }
+    function fruits(item) {
+        event.add('diet:fruits', item)
+    }
+
+    // todo hot and cold foods
 })
 
 ServerEvents.tags('fluid', event => {
     event.add('forge:milk', 'gtceu:soy_milk')
-    event.add('forge:milk', 'gtceu:milk')
-    event.add('forge:milk', 'minecraft:milk')
-})
-
-ServerEvents.tags('fluids', event => {
-    event.add('forge:milk', 'croptopia:soy_milk')
     event.add('forge:milk', 'gtceu:milk')
     event.add('forge:milk', 'minecraft:milk')
 })
