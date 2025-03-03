71e648396d8ee9bd3bfc7bddae6fcdcabe1af937
Ghostipedia <Ghostipedia@gmail.com>
2025-03-03 06:28:40 -0500

    Fixed Primordial Oil T1 being HV locked

--- overrides/kubejs/server_scripts/Recipes/Chains/PrimordialOils.js
+++ overrides/kubejs/server_scripts/Recipes/Chains/PrimordialOils.js
@@ -1,11 +1,11 @@
 ServerEvents.recipes(event => {
 
-    event.recipes.gtceu.large_chemical_reactor('prim_oil_one')
+    event.recipes.gtceu.chemical_reactor('prim_oil_one')
         .inputFluids('gtceu:primordial_oil 500')
         .inputFluids('gtceu:aether_air 500')
         .outputFluids('gtceu:lofty_primordial_oil 1000')
         .duration(160)
-        .EUt(GTValues.VA[GTValues.HV]);
+        .EUt(GTValues.VA[GTValues.MV]);
 
     event.recipes.gtceu.distillation_tower('prim_oil_one_distill')
         .inputFluids('gtceu:lofty_primordial_oil 1000')
@@ -16,6 +16,6 @@ ServerEvents.recipes(event => {
         .outputFluids('gtceu:oxygen 150')
         .outputFluids('gtceu:nitrogen 200')
         .duration(90)
-        .EUt(GTValues.VA[GTValues.HV]);
+        .EUt(GTValues.VA[GTValues.MV]);
 
 })
