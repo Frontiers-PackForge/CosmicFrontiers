47a95e9764acdd03d42fc11410007d4738010853
YoungOnion <39562198+YoungOnionMC@users.noreply.github.com>
2025-03-12 04:09:55 -0600

    Delete overrides/kubejs/server_scripts/Recipes/SurvivalOverhaul.js

--- overrides/kubejs/server_scripts/Recipes/SurvivalOverhaul.js
+++
@@ -1,9 +0,0 @@
-
-ServerEvents.compostableRecipes(event => {
-    event.add(Item.of('legendarysurvivaloverhaul:ice_fern_seeds'), 0.3)
-    event.add(Item.of('legendarysurvivaloverhaul:sun_fern_seeds'), 0.3)
-    event.add(Item.of('legendarysurvivaloverhaul:ice_fern_leaf'), 0.65)
-    event.add(Item.of('legendarysurvivaloverhaul:sun_fern_leaf'), 0.65)
-    event.add(Item.of('legendarysurvivaloverhaul:ice_fern_gold_leaf'), 1.0)
-    event.add(Item.of('legendarysurvivaloverhaul:sun_fern_gold_leaf'), 1.0)
-})
