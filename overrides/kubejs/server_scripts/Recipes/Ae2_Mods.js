cce20d607ed4b4934775e6e580a3b5c3e4e1ef6c
Ghostipedia <Ghostipedia@gmail.com>
2025-03-14 21:19:55 -0400

    The Return of ME Input hatches/busses

--- overrides/kubejs/server_scripts/Recipes/Ae2_Mods.js
+++ overrides/kubejs/server_scripts/Recipes/Ae2_Mods.js
@@ -27,8 +27,6 @@ ServerEvents.recipes(event => {
 })
 
 //Oh jeebus AE2 Recipe Yeeting
-yeet('gtceu:me_input_bus')
-yeet('gtceu:me_input_hatch')
 yeet('ae2:fluix_glass_cable')
 yeet('ae2:fluix_covered_cable')
 yeet('ae2:fluix_covered_dense_cable')
