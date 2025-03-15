9333280212c6b799eed2c14207e46c4fe2271108
Karthi Suresh <75553966+JuiceyBeans@users.noreply.github.com>
2025-03-15 17:09:21 +0000

    I cannot be bothered to learn how to rebase a closed PR. Woe, squashed commit upon ye

--- overrides/kubejs/server_scripts/Recipes/Foods/Tags.js
+++ overrides/kubejs/server_scripts/Recipes/Foods/Tags.js
@@ -1,22 +1,186 @@
-ServerEvents.tags('item', event => {
-    event.add('forge:flour', 'gtceu:wheat_dust')
-    event.add('forge:salt', 'gtceu:salt_dust')
-    event.add('forge:rice', 'farmersdelight:rice')
-    event.add('forge:cabbage', 'farmersdelight:cabbage')
-    event.add('forge:tomatoes', 'farmersdelight:tomato')
-    event.add('forge:onions', 'farmersdelight:onion')
-    event.add('frontiers:croptopia/nuts', ['croptopia:almond', 'croptopia:pecan', 'croptopia:walnut']) // non peanut nuts from croptopia used for roasted nuts recipe
-    event.add('forge:peanuts', ['vintagedelight:peanut', 'croptopia:peanut']) // this is driving me *nuts* why does one have (s)
+// priority: 999
+
+ServerEvents.tags('item', e => {
+    console.log('Registering Food Tags')
+    
+    e.add('forge:flour', 'gtceu:wheat_dust')
+    e.add('forge:salt', 'gtceu:salt_dust')
+    e.add('forge:rice', 'farmersdelight:rice')
+    e.add('forge:cabbage', 'farmersdelight:cabbage')
+    e.add('forge:tomatoes', 'farmersdelight:tomato')
+    e.add('forge:onions', 'farmersdelight:onion')
+    e.add('forge:dough', 'farmersdelight:wheat_dough')
+    e.add('forge:doughs', 'gtceu:dough')
+    
+    e.removeAll('diet:special_food') // Slicer recipes add nutrition to them already, keeping them in this tag makes it do double nutrition
+    
+    
+    spring('biomeswevegone:blueberries')
+    summer('biomeswevegone:blueberries')
+
+    // Seasonal crops
+    function spring(item) {
+        e.add('serenseseasons:spring_crops', item)
+    }
+    function summer(item) {
+        e.add('serenseseasons:summer_crops', item)
+    }
+    function autumn(item) {
+        e.add('serenseseasons:autumn_crops', item)
+    }
+    function winter(item) {
+        e.add('serenseseasons:winter_crops', item)
+    }
+
+    
+    mana('aether:blue_gummy_swet')
+    mana('aether:golden_gummy_swet')
+    mana('aether:enchanted_berry')
+    mana('malum:runic_sap')
+    mana('ars_nouveau:source_berry_roll')
+    mana('ars_nouveau:source_berry_pie')
+    mana('ars_nouveau:sourceberry_bush')
+    mana('ars_nouveau:bastion_pod')
+    mana('ars_nouveau:bombegranate_pod')
+    mana('ars_nouveau:frostaya_pod')
+    mana('ars_nouveau:mendosteen_pod')
+    mana('minecraft:enchanted_golden_apple')
+
+    psi('gtceu:basic_electronic_circuit')
+    psi('gtceu:purple_drink')
+    psi('kubejs:cosmic_meatballs')
+    psi('malum:cursed_sap')
+
+    proteins('gtceu:dough')
+    proteins('#forge:nut_butter')
+    proteins('#forge:eggs')
+    proteins('#forge:mushrooms')
+    proteins('#forge:milk')
+    proteins('nethersdelight:hoglin_loin')
+    proteins('nethersdelight:hoglin_ear')
+    proteins('nethersdelight:strider_slice')
+    proteins('occultism:beaver_nugget')
+
+    vegetables('legendarysurvivaloverhaul:beetroot_juice')
+    vegetables('legendarysurvivaloverhaul:cactus_juice')
+    vegetables('legendarysurvivaloverhaul:carrot_juice')
+    vegetables('legendarysurvivaloverhaul:golden_carrot_juice')
+    vegetables('biomeswevegone:oddion_bulb')
+    vegetables('biomeswevegone:white_puffball_cap')
+    vegetables('croptopia:tea_leaves')
+    vegetables('undergarden:mogmoss')
+    vegetables('undergarden:blue_mogmoss')
+    vegetables('undergarden:glitterkelp')
+
+    fruits('legendarysurvivaloverhaul:apple_juice')
+    fruits('legendarysurvivaloverhaul:chorus_fruit_juice')
+    fruits('legendarysurvivaloverhaul:glistering_melon_juice')
+    fruits('legendarysurvivaloverhaul:golden_apple_juice')
+    fruits('legendarysurvivaloverhaul:melon_juice')
+    fruits('legendarysurvivaloverhaul:pumpkin_juice')
+    fruits('ars_nouveau:bastion_pod')
+    fruits('ars_nouveau:bombegranate_pod')
+    fruits('ars_nouveau:frostaya_pod')
+    fruits('ars_nouveau:mendosteen_pod')
+    fruits('biomeswevegone:aloe_vera_juice')
+    fruits('biomeswevegone:baobab_fruit')
+    fruits('biomeswevegone:blueberries')
+    fruits('biomeswevegone:yucca_fruit')
+    fruits('biomeswevegone:green_apple')
+    fruits('aether:blue_berry')
+    fruits('aether:enchanted_berry')
+    fruits('aether:white_apple')
+    fruits('occultism:datura')
+    fruits('undergarden:droopvine_item')
+    fruits('nethersdelight:propelpearl')
+
+    sugars('aether:candy_cane')
+    sugars('aether:gingerbread_man')
+    sugars('create:builders_tea')
+    sugars('legendarysurvivaloverhaul:beetroot_juice')
+    sugars('legendarysurvivaloverhaul:cactus_juice')
+    sugars('legendarysurvivaloverhaul:carrot_juice')
+    sugars('legendarysurvivaloverhaul:golden_carrot_juice')
+    sugars('legendarysurvivaloverhaul:apple_juice')
+    sugars('legendarysurvivaloverhaul:chorus_fruit_juice')
+    sugars('legendarysurvivaloverhaul:glistering_melon_juice')
+    sugars('legendarysurvivaloverhaul:golden_apple_juice')
+    sugars('legendarysurvivaloverhaul:melon_juice')
+    sugars('legendarysurvivaloverhaul:pumpkin_juice')
+
+    grains('aether:gingerbread_man')
+    grains('gtceu:dough')
+
+    specialFood('create:builders_tea')
+    specialFood('supplementaries:pancake')
+    specialFood('undergarden:glitterkelp')
+    specialFood('undergarden:blue_mogmoss')
+    specialFood('undergarden:mogmoss')
+    specialFood('#forge:eggs')
+    specialFood('#forge:milk')
+    specialFood('#forge:mushrooms')
+
+    ingredients('#forge:dough')
+
+    // Dietary groups
+    function mana(item) {
+        e.add('diet:mana', item)
+    }
+    function psi(item) {
+        e.add('diet:psi', item)
+    }
+    function proteins(item) {
+        e.add('diet:proteins', item)
+    }
+    function fruits(item) {
+        e.add('diet:fruits', item)
+    }
+    function vegetables(item) {
+        e.add('diet:vegetables', item)
+    }
+    function sugars(item) {
+        e.add('diet:sugars', item)
+    }
+    function grains(item) {
+        e.add('diet:grains', item)
+    }
+    
+    // Force add food groups to items whether its edible or not. Useful for cakes, etc. but slicer recipes make this useless for placeable foods.
+    function specialFood(item) {
+        e.add('diet:special_food', item)
+    }
+    // Makes items not have food groups when eaten, but when used as ingredients for other recipes. Useful for condiments, seasonings, etc.
+    function ingredients(item) {
+        e.add('diet:ingredients', item)
+    }
+
+    // todo hot and cold foods
+
+    e.add('frontiers:croptopia/nuts', ['croptopia:almond', 'croptopia:pecan', 'croptopia:walnut']) // non peanut nuts from croptopia used for roasted nuts recipe
+    e.add('forge:peanuts', ['vintagedelight:peanut', 'croptopia:peanut']) // this is driving me *nuts* why does one have (s)
 })
 
-ServerEvents.tags('fluid', event => {
-    event.add('forge:milk', 'gtceu:soy_milk')
-    event.add('forge:milk', 'gtceu:milk')
-    event.add('forge:milk', 'minecraft:milk')
+ServerEvents.tags('block', e => {
+    // spring('biomeswevegone:blueberry_bush')
+    // summer('biomeswevegone:blueberries')
+
+    // // Seasonal crops
+    // function spring(block) {
+    //     e.add('serenseseasons:spring_crops', block)
+    // }
+    // function summer(block) {
+    //     e.add('serenseseasons:summer_crops', block)
+    // }
+    // function autumn(block) {
+    //     e.add('serenseseasons:autumn_crops', block)
+    // }
+    // function winter(block) {
+    //     e.add('serenseseasons:winter_crops', block)
+    // }
 })
 
-ServerEvents.tags('fluids', event => {
-    event.add('forge:milk', 'croptopia:soy_milk')
-    event.add('forge:milk', 'gtceu:milk')
-    event.add('forge:milk', 'minecraft:milk')
+ServerEvents.tags('fluid', e => {
+    e.add('forge:milk', 'gtceu:soy_milk')
+    e.add('forge:milk', 'gtceu:milk')
+    e.add('forge:milk', 'minecraft:milk')
 })
--- overrides/kubejs/startup_scripts/ItemModification.js
+++ overrides/kubejs/startup_scripts/ItemModification.js
@@ -1,20 +1,51 @@
 const $UUID = Java.loadClass("java.util.UUID")
 let uuid = $UUID.randomUUID();
+
 ItemEvents.modification(event => {
-    event.modify('gtceu:basic_electronic_circuit', item => {
-      item.foodProperties = food => {
-          food.hunger(6)
-          food.saturation(0.80)
-          food.fastToEat(true)
-      }
-    })
-    event.modify('kubejs:cosmic_meatballs', item => {
-      item.foodProperties = food => {
-          food.hunger(1000000)
-          food.saturation(10000)
-          food.fastToEat(false)
-      }
-    })
+    food('kubejs:cosmic_meatballs', 1000000, 1000000)    
+    food('legendarysurvivaloverhaul:apple_juice',9, 1)
+    food('legendarysurvivaloverhaul:beetroot_juice', 2, 0.9)
+    food('legendarysurvivaloverhaul:cactus_juice', 6, 0.9)
+    food('legendarysurvivaloverhaul:carrot_juice', 6, 0.9)
+    food('legendarysurvivaloverhaul:chorus_fruit_juice', 8, 0.9)
+    food('legendarysurvivaloverhaul:glistering_melon_juice', 6, 1)
+    food('legendarysurvivaloverhaul:golden_apple_juice', 8, 2.5)
+    food('legendarysurvivaloverhaul:golden_carrot_juice', 13.5, 2.5)
+    food('legendarysurvivaloverhaul:melon_juice', 4.5, 1)
+    food('legendarysurvivaloverhaul:pumpkin_juice', 6, 0.5)
+    food('croptoia:ravioli', 3, 0.9)
+
+    snackFood('gtceu:basic_electronic_circuit', 6, 0.8, true)
+
+    function snackFood(foodItem, hunger, saturation, isSnack) {
+      event.modify(foodItem, item => {
+        item.foodProperties = food => {
+            food.hunger(hunger)
+            food.saturation(saturation)
+            food.fastToEat(isSnack)
+        }
+      })
+    }
+
+    function food(foodItem, hunger, saturation) {
+      event.modify(foodItem, item => {
+        item.foodProperties = food => {
+            food.hunger(hunger)
+            food.saturation(saturation)
+            food.fastToEat(false)
+        }
+      })
+    }
+
+    function foodWithEffect(foodItem, hunger, saturation, effectId, effectDuration, effectAmplifier, effectChance) {
+      event.modify(foodItem, item => {
+        item.foodProperties = food => {
+            food.hunger(hunger)
+            food.saturation(saturation)
+            food.effect(effectId, effectDuration, effectAmplifier, effectChance)
+        }
+      })
+    }
 })
 
 ItemEvents.armorTierRegistry(event => {
@@ -65,42 +96,6 @@ ItemEvents.modification(event => {
         item.armorProtection = 6.0
         item.addAttribute("malum:soul_ward_capacity", 'b504bc09-b827-4b24-867f-553dc6dc5345', 'ward-capacity7', 7.5, "addition")
         item.addAttribute("malum:soul_ward_recovery_rate", '9b8b37d0-913e-425e-bd65-b87056f85ec3', 'recovery-rate7', 2.0, "addition")
-      })
-      //Quarktechs
-      event.modify('gtceu:quarktech_helmet', item => {
-        item.armorProtection = 6.0
-        item.addAttribute("malum:soul_ward_capacity", 'dc478cf8-9ce4-4821-958a-9aad822a8e23', 'ward-capacity7', 9.5, "addition")
-        item.addAttribute("malum:soul_ward_recovery_rate", '1dafedad-6ca8-413c-8c89-86220a891942', 'recovery-rate7', 3.0, "addition")
-      })
-      event.modify('gtceu:quarktech_chestplate', item => {
-        item.armorProtection = 10.0
-        item.addAttribute("malum:soul_ward_capacity", 'c3b224d1-b6a8-4f86-ae7e-26494d4cde92', 'ward-capacity7', 14.5, "addition")
-        item.addAttribute("malum:soul_ward_recovery_rate", 'bdfb5c91-a5cd-45d4-a5e1-52061891cc36', 'recovery-rate7', 3.0, "addition")
-      })
-      event.modify('gtceu:quarktech_leggings', item => {
-        item.armorProtection = 8.0
-        item.addAttribute("malum:soul_ward_capacity", 'f44ae125-85d0-498d-9ad0-46f74fc3c4ec', 'ward-capacity7', 9.5, "addition")
-        item.addAttribute("malum:soul_ward_recovery_rate", '2288e382-c677-419a-8c6f-2357c11b33e2', 'recovery-rate7', 3.0, "addition")
-      })
-      event.modify('gtceu:quarktech_boots', item => {
-        item.armorProtection = 6.0
-        item.addAttribute("malum:soul_ward_capacity", 'b018428b-c402-4aeb-9576-a09ed8dc60a5', 'ward-capacity7', 9.5, "addition")
-        item.addAttribute("malum:soul_ward_recovery_rate", 'b9f45ad6-00fc-4e34-823f-d614e44b8e7a', 'recovery-rate7', 3.0, "addition")
-      })
-      event.modify('cosmiccore:space_quarktech_chestplate', item => {
-        item.armorProtection = 10.0
-        item.addAttribute("malum:soul_ward_capacity", '68065d69-2242-42ee-a0db-20c30ff6e700', 'ward-capacity7', 14.5, "addition")
-        item.addAttribute("malum:soul_ward_recovery_rate", 'ba6db1b7-73a2-41ed-8bc1-e439166590e5', 'recovery-rate7', 3.0, "addition")
-      })
-      event.modify('gtceu:advanced_quarktech_chestplate', item => {
-        item.armorProtection = 10.0
-        item.addAttribute("malum:soul_ward_capacity", 'bc206f33-3d8c-44c0-a0a8-6a078492c3b0', 'ward-capacity7', 14.5, "addition")
-        item.addAttribute("malum:soul_ward_recovery_rate", '9f531419-d739-496d-9f3a-40af3eb9c894', 'recovery-rate7', 3.0, "addition")
-      })
-      event.modify('cosmiccore:space_advanced_quarktech_chestplate', item => {
-        item.armorProtection = 10.0
-        item.addAttribute("malum:soul_ward_capacity", 'aa3c7f47-6df3-47ac-a349-a98b457f04e4', 'ward-capacity7', 14.5, "addition")
-        item.addAttribute("malum:soul_ward_recovery_rate", '7bb55156-4d51-424a-b9a1-e3bdf565bfff', 'recovery-rate7', 3.0, "addition")
       })
   
     })
