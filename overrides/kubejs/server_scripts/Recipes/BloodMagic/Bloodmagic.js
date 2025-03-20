34458221c7ec89abe642a80dd9b0c014c9d09aa3
Ghostipedia <Ghostipedia@gmail.com>
2025-03-20 13:08:15 -0400

    All <5 Grading Catalysts Are LuV

--- overrides/kubejs/server_scripts/Recipes/BloodMagic/Bloodmagic.js
+++ overrides/kubejs/server_scripts/Recipes/BloodMagic/Bloodmagic.js
@@ -187,17 +187,17 @@ ServerEvents.recipes(event => {
   event.recipes.gtceu.mana_engraver('frontiers:grading_fluid_3')
     .notConsumable('bloodmagic:magicianbloodorb')
     .itemInputs(['64x gtceu:graphene_dust', '64x gtceu:polydimethylsiloxane_dust', '4x cosmiccore:resonant_virtue_meld_dust'])
-    .inputFluids('gtceu:grading_fluid_2 1000', 'gtceu:genesis_resin 2000', 'gtceu:neutronite 1152', 'gtceu:potent_mana 16000', 'cosmiccore:prisma 32000')
+    .inputFluids('gtceu:grading_fluid_2 1000', 'gtceu:lake_water 2000', 'gtceu:neutronite 1152', 'gtceu:potent_mana 16000', 'cosmiccore:prisma 32000')
     .outputFluids('gtceu:grading_fluid_3 1000')
     .duration(1500)
-    .EUt(GTValues.VA[GTValues.ZPM]);
+    .EUt(GTValues.VA[GTValues.LuV]);
   event.recipes.gtceu.mana_engraver('frontiers:grading_fluid_4')
     .notConsumable('bloodmagic:magicianbloodorb')
     .itemInputs(['64x gtceu:graphene_dust', '64x gtceu:4_4_oxydianiline_pyromellitimide_dust', '8x botania:life_essence'])
-    .inputFluids('gtceu:grading_fluid_3 1000', 'gtceu:void_tar 8000', 'cosmiccore:trinavine 1152', 'gtceu:potent_mana 32000', 'cosmiccore:prisma 64000')
+    .inputFluids('gtceu:grading_fluid_3 1000', 'gtceu:sterilized_lake_water 8000', 'cosmiccore:trinavine 1152', 'gtceu:potent_mana 32000', 'cosmiccore:prisma 64000')
     .outputFluids('gtceu:grading_fluid_4 1000')
     .duration(150)
-    .EUt(GTValues.VA[GTValues.ZPM]);
+    .EUt(GTValues.VA[GTValues.LuV]);
 
   event.recipes.gtceu.hellfire_foundry('frontiers:tau_oil')
     .itemInputs(['bloodmagic:weak_tau', 'bloodmagic:weak_tau', 'bloodmagic:weak_tau', 'gtceu:oilsands_dust', 'bloodmagic:reagentbinding'])
