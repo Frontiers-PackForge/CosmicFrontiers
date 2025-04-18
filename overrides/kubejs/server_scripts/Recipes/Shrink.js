7162a96fbf70af43edfba28ad256e2c860ac9fe2
AWildVolt <166110881+AWildVolt@users.noreply.github.com>
2025-04-19 00:18:18 +1200

    forgor 💀

--- overrides/kubejs/server_scripts/Recipes/Shrink.js
+++ overrides/kubejs/server_scripts/Recipes/Shrink.js
@@ -2,7 +2,7 @@ ServerEvents.recipes(event => {
 
     event.remove({ output: 'shrink:shrinking_device' })
     event.recipes.gtceu.assembler('shrinking_device')
-    .itemInputs(['gtceu:lv_field_generator', '4x gtceu:double_steel_plate', '4x gtceu:glass_plate', 'gtceu:ender_pearl_plate'])
+    .itemInputs(['gtceu:lv_field_generator', '4x gtceu:double_steel_plate', '4x gtceu:glass_plate', 'gtceu:ender_pearl_plate', '#gtceu:batteries/lv'])
     .inputFluids('gtceu:soldering_alloy 288')
     .itemOutputs('shrink:shrinking_device')
     .duration(200)
