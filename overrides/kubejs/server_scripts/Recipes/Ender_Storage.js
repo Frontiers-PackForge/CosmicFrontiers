d8fbfd231652d5aea96605e2b41ad3782c4a747a
AWildVolt <166110881+AWildVolt@users.noreply.github.com>
2025-05-15 01:22:14 +1200

    gregify ender buckets/bags (#400)

--- overrides/kubejs/server_scripts/Recipes/Ender_Storage.js
+++ overrides/kubejs/server_scripts/Recipes/Ender_Storage.js
@@ -7,11 +7,38 @@ ServerEvents.recipes( event =>{
     .duration(140)
     .EUt(GTValues.VA[GTValues.MV])
 
+    //links to an ender tank from the mod
+    event.remove({ output: 'endertanks:ender_bucket' })
+    event.recipes.gtceu.assembler('enderbucket')
+    .itemInputs(['2x #gtceu:circuits/mv', '4x gtceu:obsidian_plate', 'gtceu:aluminium_drum', '1x gtceu:ender_pearl_plate'])
+    .inputFluids('gtceu:blaze 1152')
+    .itemOutputs('endertanks:ender_bucket')
+    .duration(140)
+    .EUt(GTValues.VA[GTValues.MV])
+
     event.remove({ output: 'enderchests:ender_chest' })
     event.recipes.gtceu.assembler('enderchest')
     .itemInputs(['2x #gtceu:circuits/hv', '4x gtceu:obsidian_plate', '1x gtceu:lv_super_chest', '1x gtceu:ender_pearl_plate'])
     .inputFluids('gtceu:blaze 2304')
     .itemOutputs('enderchests:ender_chest')
+    .duration(140)
+    .EUt(GTValues.VA[GTValues.MV])
+ 
+    //lets you access the vanilla enderchest remotely
+    event.remove({ output: 'enderchests:ender_pouch' }) 
+    event.recipes.gtceu.assembler('enderpouch')
+    .itemInputs(['2x #gtceu:circuits/mv', '4x cosmiccore:waxed_leather', 'minecraft:ender_chest', '1x gtceu:ender_pearl_plate'])
+    .inputFluids('gtceu:blaze 1152')
+    .itemOutputs('enderchests:ender_pouch')
+    .duration(140)
+    .EUt(GTValues.VA[GTValues.MV])
+
+    //links to an ender chest from the mod
+    event.remove({ output: 'enderchests:ender_bag' })
+    event.recipes.gtceu.assembler('enderbag')
+    .itemInputs(['2x #gtceu:circuits/mv', '4x gtceu:obsidian_plate', 'gtceu:aluminium_crate', '1x gtceu:ender_pearl_plate'])
+    .inputFluids('gtceu:blaze 1152')
+    .itemOutputs('enderchests:ender_bag')
     .duration(140)
     .EUt(GTValues.VA[GTValues.MV])
 })
