94d92e19560505d8aada394d372e56d175873b98
txnug <57244929+txnug@users.noreply.github.com>
2025-07-05 19:38:10 -0500

    Update Gregtech_Assline.js

--- overrides/kubejs/server_scripts/Recipes/Gregtech/Gregtech_Assline.js
+++ overrides/kubejs/server_scripts/Recipes/Gregtech/Gregtech_Assline.js
@@ -29,8 +29,8 @@ ServerEvents.recipes(event => {
       'gtceu:grading_fluid_1 32000',
       'gtceu:grading_fluid_2 16000'
     )
-  ["scannerResearch(java.util.function.UnaryOperator)"](b => b.researchStack(Item.of('occultism:chalk_red', '{Damage:0}')).EUt(GTValues.VA[GTValues.LuV]).duration(23800))
-    .duration(300)
+  ["scannerResearch(java.util.function.UnaryOperator)"](b => b.researchStack(Item.of('occultism:chalk_red', '{Damage:0}')).EUt(GTValues.VA[GTValues.LuV]).duration(3200))
+    .duration(1200)
     .EUt(GTValues.VA[GTValues.LuV])
 
   event.recipes.gtceu.assembly_line('luv_hull_assline')
