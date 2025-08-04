d17cda00e03feaf52a05d7d48bf42d8b31aa8e78
MrQuentinet <32539717+mrquentin@users.noreply.github.com>
2025-08-04 16:42:02 -0400

    Update action to trigger on release published

--- scripts/release.py
+++ scripts/release.py
@@ -6,9 +6,8 @@ from server_generation import generate_serverpack_zip
 
 def main():
     version = os.environ.get('RELEASE_VERSION')
-    prerelease = os.environ.get('RELEASE_PRERELEASE')
     curseforge_api_key = os.environ.get('CURSEFORGE_API_KEY')
-    print(f"Release script running. Version: {version}, Pre-release: {prerelease}")
+    print(f"Release script running. Version: {version}")
     generate_modpack_zip(version)
     generate_serverpack_zip(version, curseforge_api_key=curseforge_api_key)
 
