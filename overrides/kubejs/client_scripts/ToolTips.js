d0bbfde72189c490b2e0ff7086a38b4ca7aa2cb7
AWildVolt <166110881+AWildVolt@users.noreply.github.com>
2025-09-07 03:01:57 +1200

    make tanks need GT items instead of ender pearls
    also tooltips

--- overrides/kubejs/client_scripts/ToolTips.js
+++ overrides/kubejs/client_scripts/ToolTips.js
@@ -339,6 +339,24 @@ ItemEvents.tooltip(event => {
   event.addAdvanced('legendarysurvivaloverhaul:purified_water_bottle', (item, advanced, text) => {
     text.add(1, Text.of('Recipes Do Not Work In Iron Furnaces').red())
   })
+  //ender tanks/chests
+  event.addAdvanced('endertanks:ender_tank', (item, advanced, text) => {
+    text.add(1, Text.of('Requires GT Components To Upgrade').blue().italic())
+    if (event.isShift()) {
+    text.add(2, [Text.of('Capacity: ').aqua(), Text.of('LV Field Generator, 8B Per').gray(), Text.of(' Or ').gold(), Text.of('MV Field Generator, 32B Per, Max').gray().gray(), Text.of(' 256B ').gold()])
+    text.add(3, [Text.of('Pump Speed: ').aqua(), Text.of('LV Electric Piston, 1B Per, Max').gray(), Text.of(' 4B ').gold()])
+    } else {
+      text.add(2, [Text.of('Hold ').gold(), Text.of('Shift ').yellow(), Text.of('to see more info.').gold()])
+    }
+  })
+  event.addAdvanced('enderchests:ender_chest', (item, advanced, text) => {
+    text.add(1, Text.of('Requires GT Components To Upgrade').blue().italic())
+    if (event.isShift()) {
+    text.add(2, [Text.of('Inventory Slots: ').aqua(), Text.of('LV Field Generator, 3 Slots Per').gray(), Text.of(' Or ').gold(), Text.of('MV Field Generator, 9 Slots Per, Max').gray(), Text.of(' 27 ').gold()])
+    } else {
+      text.add(2, [Text.of('Hold ').gold(), Text.of('Shift ').yellow(), Text.of('to see more info.').gold()])
+    }
+  })
 
 })
 
