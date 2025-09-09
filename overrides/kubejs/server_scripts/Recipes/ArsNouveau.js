9ec6f4fdefe6c82f74575d557ac5b1da7150d648
AWildVolt <166110881+AWildVolt@users.noreply.github.com>
2025-09-09 12:19:10 +1200

    change ars spellbook recipes to default, late/mid lv, late/mid mv, add pity recipe for wilden ritual tablet (#587)
    * change recipes to default, late lv, late mv
    
    * add pity recipe for wilden ritual
    
    uhh merge conflicts just putting it in this pr
    
    * remove magebloom cutting recipe
    
    also a couple not needed anymore recipe removals (in the occultism file)

--- overrides/kubejs/server_scripts/Recipes/ArsNouveau.js
+++ overrides/kubejs/server_scripts/Recipes/ArsNouveau.js
@@ -27,9 +27,8 @@ ServerEvents.recipes(event => {
         ],
         "sourceCost": 2000
       })
-    event.remove({id:'occultism:ritual/craft_stabilizer_tier3'})
-    event.remove({id:'ars_ocultas:craft_stabilizer_tier4'})
-    event.remove({id:'occultism:ritual/craft_stabilizer_tier4'})
+
+    event.remove({id:'delightful:integration/ars_nouveau/cutting/magebloom'})
     event.remove({ output: 'ars_nouveau:archwood_chest' })
     event.remove({ output: 'arseng:source_cell_housing' })
 
@@ -311,57 +310,17 @@ ServerEvents.recipes(event => {
     }
     )
     //spellbooks
-  .id("malum:spirit_infusion.frontiers.novice_spell_book")
-  event.remove({ id: 'ars_nouveau:novice_spell_book' })
-  event.custom(
-    {
-      "type": "malum:spirit_infusion",
-      "extra_items": [
-        {
-          "count": 1,
-          "item": "botania:manasteel_sword"
-        },
-        {
-          "count": 1,
-          "item": "botania:manasteel_pick"
-        },
-        {
-          "count": 1,
-          "item": "botania:manasteel_axe"
-        },
-        {
-          "count": 1,
-          "item": "botania:manasteel_shovel"
-        },
-      ],
-      "input": {
-        "count": 1,
-        "item": "minecraft:book"
-      },
-      "output": {
-        "count": 1,
-        "item": "ars_nouveau:novice_spell_book"
-      },
-      "spirits": [
-        {
-          "type": "arcane",
-          "count": 32
-        },
-        {
-          "type": "aqueous",
-          "count": 16
-        },
-        {
-          "type": "aerial",
-          "count": 16
-        },
-        {
-          "type": "sacred",
-          "count": 16
-        },
-      ]
-    }
-  )
+    //incase an occultism recipe is wanted instead
+  //event.remove({ id: 'ars_nouveau:novice_spell_book' })
+  //event.recipes.occultism.ritual(
+  //  'ars_nouveau:novice_spell_book',
+  //  ['minecraft:iron_sword',
+  //    'minecraft:iron_pickaxe',
+  //    'minecraft:iron_axe',
+  //    'minecraft:iron_shovel',],
+  //  'minecraft:book',
+  //  'occultism:rune_ritual'
+  //).dummy("kubejs:dummy_ritual_thing").id("occultism:frontiers.novice_spell_book_ritual")
 
   .id("malum:spirit_infusion.frontiers.apprentice_spell_book")
   event.remove({ id: 'ars_nouveau:apprentice_spell_book_upgrade' })
@@ -371,11 +330,11 @@ ServerEvents.recipes(event => {
       "extra_items": [
         {
           "count": 8,
-          "item": "botania:mana_diamond"
+          "item": "malum:spirit_fabric"
         },
         {
           "count": 8,
-          "item": "kubejs:terraweave_cloth"
+          "item": "botania:manasteel_ingot"
         },
         {
           "count": 1,
@@ -396,7 +355,11 @@ ServerEvents.recipes(event => {
           "count": 32
         },
         {
-          "type": "infernal",
+          "type": "aerial",
+          "count": 16
+        },
+        {
+          "type": "aqueous",
           "count": 16
         },
         {
@@ -415,11 +378,11 @@ ServerEvents.recipes(event => {
       "extra_items": [
         {
           "count": 8,
-          "item": "gtceu:quantum_eye"
+          "item": "kubejs:terraweave_cloth"
         },
         {
           "count": 8,
-          "item": "gtceu:luminescent_utherium_ingot"
+          "item": "gtceu:galvanized_ethersteel_ingot"
         },
         {
           "count": 1,
@@ -441,6 +404,56 @@ ServerEvents.recipes(event => {
         },
         {
           "type": "eldritch",
+          "count": 16
+        },
+        {
+          "type": "wicked",
+          "count": 16
+        },
+        {
+          "type": "infernal",
+          "count": 16
+        },
+      ]
+    }
+  )
+  //pity recipe cause the wilden dens spawn *very* rarely, should be a one time craft
+  event.custom(
+    {
+      "type": "malum:spirit_infusion",
+      "extra_items": [
+        {
+          "count": 8,
+          "item": "minecraft:phantom_membrane"
+        },
+        {
+          "count": 8,
+          "item": "minecraft:rotten_flesh"
+        },
+        {
+          "count": 1,
+          "item": "minecraft:wither_skeleton_skull"
+        },
+      ],
+      "input": {
+        "count": 1,
+        "item": "ars_nouveau:purple_archwood_log"
+      },
+      "output": {
+        "count": 1,
+        "item": "ars_nouveau:ritual_wilden_summon"
+      },
+      "spirits": [
+        {
+          "type": "arcane",
+          "count": 32
+        },
+        {
+          "type": "earthen",
+          "count": 32
+        },
+        {
+          "type": "aerial",
           "count": 16
         },
         {
