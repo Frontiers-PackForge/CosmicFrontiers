e32b446aa3fbaad3c432d7aeb61848dba172d453
AWildVolt <166110881+AWildVolt@users.noreply.github.com>
2025-09-09 12:19:24 +1200

    add back a blazing quartz recipe (#588)
    * add back a blazing quartz recipe
    
    * replace eye of ender for mana pearl for the necklace of the mystic mirror
    
    (would have done this in a seperate pr but merge conflicts)
    
    * up blaze cost a tiny bit and make 1:1

--- overrides/kubejs/server_scripts/Recipes/Malum.js
+++ overrides/kubejs/server_scripts/Recipes/Malum.js
@@ -201,7 +201,7 @@ ServerEvents.recipes(event => {
         },
       ]
     }
-  //chorus (one of the flower recipes doesnt work so just gonna make it output a flower)
+  //chorus 
   ).id("malum:spirit_infusion.frontiers.chorus_flower")
 
 
@@ -274,6 +274,73 @@ ServerEvents.recipes(event => {
     'occultism:rune_ritual'
   ).dummy("kubejs:dummy_ritual_thing").id("occultism:frontiers.cthonic_gold_ritual")
 
+  //blazing quartz
+  .id("malum:spirit_infusion.frontiers.blazing_quartz")
+  event.custom(
+    {
+      "type": "malum:spirit_infusion",
+      "extra_items": [
+        {
+          "count": 4,
+          "item": "minecraft:blaze_powder"
+        },
+      ],
+      "input": {
+        "count": 32,
+        "item": "minecraft:quartz"
+      },
+      "output": {
+        "count": 32,
+        "item": "malum:blazing_quartz"
+      },
+      "spirits": [
+        {
+          "type": "arcane",
+          "count": 16
+        },
+        {
+          "type": "infernal",
+          "count": 16
+        },
+      ]
+    }
+  )
+  //necklace of the mystic mirror (one of the only malum curios that is gated to hv because of an eye of ender)
+  .id("malum:spirit_infusion.frontiers.necklace_of_the_mystic_mirror")
+  event.remove({ id: 'malum:spirit_infusion/necklace_of_the_mystic_mirror' })
+  event.custom(
+    {
+      "type": "malum:spirit_infusion",
+      "extra_items": [
+        {
+          "count": 8,
+          "item": "malum:runewood_planks"
+        },
+        {
+          "count": 1,
+          "item": "malum:spectral_optic"
+        },
+        {
+          "count": 1,
+          "item": "botania:mana_pearl"
+        },
+      ],
+      "input": {
+        "count": 1,
+        "item": "malum:ornate_necklace"
+      },
+      "output": {
+        "count": 1,
+        "item": "malum:necklace_of_the_mystic_mirror"
+      },
+      "spirits": [
+        {
+          "type": "sacred",
+          "count": 24
+        },
+      ]
+    }
+  )
   //impetus/spirit crucible removal
   event.remove({ id: /malum:spirit_crucible(.*)/ })
   event.remove({ id: /malum:impetus_creation(.*)/ })
@@ -311,5 +378,4 @@ ServerEvents.recipes(event => {
 
 // Mass Removal
 console.log('[Malum.js finished loading]')
-
 
