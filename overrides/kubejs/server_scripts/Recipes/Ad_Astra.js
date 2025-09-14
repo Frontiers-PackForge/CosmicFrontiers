e2c75ae81e43c2a7e3c03c8367f06805db61a82d
JuiceyBeans <75553966+JuiceyBeans@users.noreply.github.com>
2025-09-14 15:49:44 +0100

    Replace Ad Astra tag references with GT items

--- overrides/kubejs/server_scripts/Recipes/Ad_Astra.js
+++ overrides/kubejs/server_scripts/Recipes/Ad_Astra.js
@@ -425,6 +425,26 @@ ServerEvents.recipes(event => {
     'ad_astra:desh_block'
   )
   event.replaceInput(
+    { input: '#ad_astra:steel_block' },
+    '#ad_astra:steel_blocks',
+    'gtceu:steel_block'
+  )
+  event.replaceInput(
+    { input: '#ad_astra:iron_plates' },
+    '#ad_astra:iron_plates',
+    'gtceu:iron_plate'
+  )
+  event.replaceInput(
+    { input: '#ad_astra:steel_plates' },
+    '#ad_astra:steel_plates',
+    'gtceu:steel_plate'
+  )
+  event.replaceInput(
+    { input: '#ad_astra:steel_ingots' },
+    '#ad_astra:steel_ingots',
+    'gtceu:steel_ingot'
+  )
+  event.replaceInput(
     { input: 'ad_astra:iron_rod' },
     'ad_astra:iron_rod',
     'gtceu:iron_rod'
