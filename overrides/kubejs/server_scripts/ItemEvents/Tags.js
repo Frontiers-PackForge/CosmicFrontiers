eaa997420875cc3e27c267dab9a42d1c9d685178
CatPlasma <110224489+Koamasi@users.noreply.github.com>
2025-11-12 19:50:17 +0800

    Update Tags.js

--- overrides/kubejs/server_scripts/ItemEvents/Tags.js
+++ overrides/kubejs/server_scripts/ItemEvents/Tags.js
@@ -1,15 +1,7 @@
 ServerEvents.tags("item", event =>{
     //Add Ember augment support for the CosmicCore chestplate
-    event.add("embers:augmentables/tools_armors", [
-        '/cosmiccore:.*chestplate/'
-    ]);
-    event.add("embers:augmentables", [
-        '/cosmiccore:.*chestplate/'
-    ]);
-    event.add("embers:augmentables/armors", [
-        '/cosmiccore:.*chestplate/'
-    ]);
-    event.add("embers:augmentables/armors/chestplates", [
-        '/cosmiccore:.*chestplate/'
-    ]);
+    event.add("embers:augmentables/tools_armors", ['/cosmiccore:.*chestplate/']);
+    event.add("embers:augmentables", ['/cosmiccore:.*chestplate/']);
+    event.add("embers:augmentables/armors", ['/cosmiccore:.*chestplate/']);
+    event.add("embers:augmentables/armors/chestplates", ['/cosmiccore:.*chestplate/']);
 })
