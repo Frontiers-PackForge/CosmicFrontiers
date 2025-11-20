ed685ab5988cc5abf21c8a9cde4ac62f737d5b1f
JuiceyBeans <75553966+JuiceyBeans@users.noreply.github.com>
2025-11-20 17:57:53 +0000

    Very many recipe removals and fixes

--- overrides/kubejs/server_scripts/Recipes/InitRecipeRemovals.js
+++ overrides/kubejs/server_scripts/Recipes/InitRecipeRemovals.js
@@ -35,4 +35,91 @@ ServerEvents.recipes(e => {
     e.remove({id: 'undergarden:stonecutter_from_depthrock'})
     e.remove({id: 'undergarden:stonecutter_from_shiverstone'})
     e.remove({id: 'undergarden:stonecutter_from_tremblecrust'})
+    
+    e.remove('tconstruct:common/basalt_blast_furnace')
+    e.remove('farmersdelight:scaffolding_from_canvas')
+    e.remove('undergarden:undergarden_scaffolding')
+    e.remove('create:crafting/appliances/item_frame')
+    e.remove('minecraft:target')
+    e.remove('tconstruct:common/glass/vanilla/daylight_detector')
+    e.remove('tconstruct:common/slime/sticky_piston')
+    e.remove('embers:sticky_piston_adhesive')
+    e.remove('undergarden:sticky_piston_from_goo_ball')
+    e.remove('aether:swet_sticky_piston')
+    e.remove('gtceu:shaped/bamboo_chest_raft')
+    e.remove('dustydecoration:lead_recipe')
+    e.remove('tconstruct:common/slime/lead')
+    e.remove('embers:lead_adhesive')
+    e.remove('undergarden:undergarden_lead')
+    e.remove('aether:swet_lead')
+    e.remove('farmersdelight:lead_from_straw')
+    e.remove('farmersdelight:cake_from_milk_bottle')
+    e.remove('forestry:cake_can')
+    e.remove('forestry:cake_capsule')
+    e.remove('forestry:cake_refractory')
+    e.remove('vanilladelight:pumpkin_pie_from_slices')
+    e.remove('delightful:food/cookie')
+    e.remove('botania:mushroom_stew')
+    e.remove('biomesoplenty:rabbit_stew_from_toadstool')
+    e.remove('crabbersdelight:scute_from_block')
+
+    e.replaceInput({ id: 'gtceu:shaped/scaffolding' },
+        'minecraft:slime_ball',
+        '#forge:slimeballs'
+    )
+
+    e.replaceInput({ id: 'gtceu:shaped/sticky_piston' },
+        'minecraft:string',
+        ['minecraft:string', 'undergarden:twsity_twig']
+    )
+
+    e.replaceInput({ id: 'create:crafting/appliances/name_tags' },
+        'create:cardboard',
+        ['create:cardboard', 'farmersdelight:canvas']
+    )
+
+    e.replaceInput({ id: 'gtceu:shaped/lead' },
+        'minecraft:string',
+        ['minecraft:string', 'farmersdelight:straw']
+    )
+
+    e.replaceInput({ id: 'gtceu:shaped/lead' },
+        'minecraft:slime_ball',
+        '#forge:slimeballs'
+    )
+
+    e.replaceInput({ id: 'gtceu:assembler/lead' },
+        'minecraft:string',
+        ['minecraft:string', 'farmersdelight:straw']
+    )
+
+    e.replaceInput({ id: 'gtceu:assembler/lead' },
+        'minecraft:slime_ball',
+        '#forge:slimeballs'
+    )
+
+    e.shaped('8x minecraft:scaffolding', [
+        'BSB',
+        'BWB',
+        'BMB'
+    ], {
+        B: 'minecraft:bamboo',
+        S: 'farmersdelight:canvas',
+        W: 'gtceu:wood_bolt',
+        M: '#forge:tools/mallets'
+    })
+
+    e.shapeless('minecraft:bamboo_chest_raft', ['minecraft:bamboo_raft', '#forge:chests/wooden'])
+
+    e.recipes.gtceu.assembler(`frontiers:scaffolding_from_canvas`)
+        .itemInputs(['6x minecraft:bamboo', 'farmersdelight:canvas'])
+        .itemOutputs('8x minecraft:scaffolding')
+        .duration(100)
+        .EUt(GTValues.VA[GTValues.ULV]);
+
+    e.recipes.gtceu.assembler(`frontiers:item_frame_from_cardboard`)
+        .itemInputs(['8x minecraft:stick', 'create:cardboard'])
+        .itemOutputs('2x minecraft:item_frame')
+        .duration(100)
+        .EUt(GTValues.VA[GTValues.ULV]);
 })
