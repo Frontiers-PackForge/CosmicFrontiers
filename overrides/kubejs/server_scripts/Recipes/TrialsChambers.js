0e7bda310934d1622ff74bb2ba80fa70c46ec981
JuiceyBeans <75553966+JuiceyBeans@users.noreply.github.com>
2025-11-21 12:50:46 +0000

    Creator disc

--- overrides/kubejs/server_scripts/Recipes/TrialsChambers.js
+++ overrides/kubejs/server_scripts/Recipes/TrialsChambers.js
@@ -23,14 +23,21 @@ ServerEvents.recipes(event => {
     })
 
     event.recipes.gtceu.assembler(`frontiers:copper_bulb`)
-        .itemInputs(['3x minecraft:copper_block', 'minecraft:blaze_rod', 'projectred_core:conductive_plate'])
+        .itemInputs('3x minecraft:copper_block', 'minecraft:blaze_rod', 'projectred_core:conductive_plate')
         .itemOutputs('6x trials:copper_bulb')
         .duration(400)
-        .EUt(GTValues.VA[GTValues.ULV]);
+        .EUt(GTValues.VA[GTValues.LV]);
 
     event.recipes.gtceu.assembler(`frontiers:tuff_bulb`)
-        .itemInputs(['3x trials:polished_tuff', 'minecraft:blaze_rod', 'projectred_core:conductive_plate'])
+        .itemInputs('3x trials:polished_tuff', 'minecraft:blaze_rod', 'projectred_core:conductive_plate')
         .itemOutputs('6x trials:chiseled_tuff_bulb')
         .duration(400)
-        .EUt(GTValues.VA[GTValues.ULV]);
+        .EUt(GTValues.VA[GTValues.LV]);
+
+    event.recipes.gtceu.chemical_reactor(`frontiers:music_disc_creator`)
+        .itemInputs('trials:music_disc_creator_box')
+        .fluidInputs('gtceu:oxygen 500')
+        .itemOutputs('trials:music_disc_creator')
+        .duration(220)
+        .EUt(GTValues.VA[GTValues.LV]);
 })
