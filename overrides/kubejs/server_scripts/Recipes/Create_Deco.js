63b030727269f421708f8ca6e31e90667b63dba9
JuiceyBeans <75553966+JuiceyBeans@users.noreply.github.com>
2025-11-21 13:05:34 +0000

    Revert "Create deco doors and trapdoors"
    This reverts commit d6be948628a4e161763f8819dbd68e1d40c4bdb7.

--- overrides/kubejs/server_scripts/Recipes/Create_Deco.js
+++ overrides/kubejs/server_scripts/Recipes/Create_Deco.js
@@ -70,61 +70,16 @@ ServerEvents.recipes(event => {
         'gtceu:zinc_ingot'  
     )
 
-    event.recipes.gtceu.alloy_smelter("create:industrial_iron_ingot")
-        .itemInputs('4x minecraft:iron_ingot', '2x gtceu:coal_dust')
-        .itemOutputs('4x createdeco:industrial_iron_ingot')
-        .duration(100)
-        .EUt(16);
+  event.recipes.gtceu.alloy_smelter("create:industrial_iron_ingot")
+    .itemInputs('4x minecraft:iron_ingot', '2x gtceu:coal_dust')
+    .itemOutputs('4x createdeco:industrial_iron_ingot')
+    .duration(100)
+    .EUt(16);
 
-    event.recipes.gtceu.mixer("gtceu:industrial_iron_dust")
-        .itemInputs('4x gtceu:iron_dust', '2x gtceu:coal_dust')
-        .circuit(1)
-        .itemOutputs('4x gtceu:industrial_iron_dust')
-        .duration(50)
-        .EUt(16);
-        
-    function trapdoor(trapdoor, slab) {
-        event.remove(`${trapdoor}`)
-        event.shaped(`2x ${trapdoor}`, [
-            'SSS',
-            'SSS'
-        ], {
-            S: `${slab}`
-        })
-    }
-
-    function door(block, material, trapdoor) {
-        event.remove(`${block}`)
-        event.shaped(`2x ${block}`, [
-            'PTD',
-            'PRS',
-            'PPH'
-        ], {
-            P: `gtceu:${material}_plate`,
-            T: `${trapdoor}`,
-            D: '#forge:tools/screwdrivers',
-            R: `gtceu:iron_ring`,
-            S: `gtceu:iron_screw`,
-            H: '#forge:tools/hammers'
-        })
-    }
-    
-    trapdoor('createdeco:andesite_trapdoor', 'create:andesite_alloy')
-    trapdoor('createdeco:brass_trapdoor', 'gtceu:brass_plate')
-    trapdoor('createdeco:copper_trapdoor', 'gtceu:copper_plate')
-    trapdoor('createdeco:industrial_iron_trapdoor', 'gtceu:industrial_iron_plate')
-    trapdoor('createdeco:zinc_trapdoor', 'gtceu:zinc_plate')
-    trapdoor('trials:copper_trapdoor', 'gtceu:copper_plate')
-    
-    door('createdeco:andesite_door', 'andesite_alloy', 'createdeco:andesite_trapdoor')
-    door('createdeco:brass_door', 'brass', 'createdeco:brass_trapdoor')
-    door('createdeco:copper_door', 'copper', 'createdeco:copper_trapdoor')
-    door('createdeco:industrial_iron_door', 'industrial_iron', 'createdeco:industrial_iron_trapdoor')
-    door('createdeco:zinc_door', 'zinc', 'createdeco:zinc_trapdoor')
-    door('trials:copper_door', 'copper', 'gtceu:copper_plate')
-    
-    event.stonecutting('createdeco:copper_door', 'trials:copper_door')
-    event.stonecutting('trials:copper_door', 'createdeco:copper_door')
-    event.stonecutting('createdeco:copper_trapdoor', 'trials:copper_trapdoor')
-    event.stonecutting('trials:copper_trapdoor', 'createdeco:copper_trapdoor')
+  event.recipes.gtceu.mixer("gtceu:industrial_iron_dust")
+    .itemInputs('4x gtceu:iron_dust', '2x gtceu:coal_dust')
+    .circuit(1)
+    .itemOutputs('4x gtceu:industrial_iron_dust')
+    .duration(50)
+    .EUt(16);
 })
