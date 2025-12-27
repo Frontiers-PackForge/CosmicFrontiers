467cd1d81db0ea953db6c05cbaeda93f6ca6d9c7
Ghostipedia <Ghostipedia@gmail.com>
2025-12-26 23:22:11 -0500

    Rock and Stone (StoneTypes)

---
+++ overrides/kubejs/startup_scripts/WorldGenRegistry/StoneTypes.js
@@ -0,0 +1,76 @@
+StartupEvents.registry('gtceu:tag_prefix', e => {
+    const JBoolean = Java.loadClass("java.lang.Boolean")
+
+    function asId(s) {
+        s = String(s)
+        let i = s.indexOf(':')
+        return i >= 0 ? { namespace: s.substring(0, i), path: s.substring(i + 1) }
+            : { namespace: 'minecraft', path: s }
+    }
+
+    function createStoneTypeOre(type, properties) {
+        if (properties === undefined) properties = {}
+
+        const id = asId(type)
+
+        if (properties.baseModel === undefined)
+            properties.baseModel = id.namespace + ":block/" + id.path
+
+        if (properties.material === undefined)
+            properties.material = null
+
+        if (properties.blockState === undefined)
+            properties.blockState = () => Block.getBlock(type).defaultBlockState()
+
+        e.create(id.path, 'ore')
+            .stateSupplier(properties.blockState)
+            .baseModelLocation(properties.baseModel)
+            .materialSupplier(properties.material)
+            .unificationEnabled(true)
+            .materialIconType(GTMaterialIconType.ore)
+            .miningToolTag("minecraft:mineable/pickaxe")
+            .generationCondition(ItemGenerationCondition.hasOreProperty)
+    }
+
+    // Sane Ores because apparently ore blocks need to be made esoteric in half of these mods.
+
+    //Undergarden
+    createStoneTypeOre('undergarden:depthrock')
+    createStoneTypeOre('undergarden:shiverstone')
+    createStoneTypeOre('undergarden:sediment')
+    createStoneTypeOre('minecraft:soul_soil')
+
+
+    createStoneTypeOre('aether:holystone', {
+        blockState: () => {
+            const state = Block.getBlock('aether:holystone').defaultBlockState()
+            const props = state.getProperties()
+            for (let p of props) {
+                if (String(p.getName()) == "double_drops") return state.setValue(p, JBoolean.TRUE)
+            }
+            return state
+        }
+    })
+
+    createStoneTypeOre('aether:mossy_holystone', {
+        blockState: () => {
+            const state = Block.getBlock('aether:mossy_holystone').defaultBlockState()
+            const props = state.getProperties()
+            for (let p of props) {
+                if (String(p.getName()) == "double_drops") return state.setValue(p, JBoolean.TRUE)
+            }
+            return state
+        }
+    })
+
+    createStoneTypeOre('aether:icestone')
+
+    // Create stones
+    createStoneTypeOre('create:asurine', { baseModel: 'create:block/asurine_natural_0' })
+    createStoneTypeOre('create:crimsite', { baseModel: 'create:block/crimsite_natural_0' })
+    createStoneTypeOre('create:limestone')
+    createStoneTypeOre('create:ochrum', { baseModel: 'create:block/ochrum_natural_0' })
+    createStoneTypeOre('create:scoria')
+    createStoneTypeOre('create:scorchia')
+    createStoneTypeOre('create:veridium', { baseModel: 'create:block/veridium_natural_0' })
+})
