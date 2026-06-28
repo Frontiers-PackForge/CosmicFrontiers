5895d684ee9b22eba0bd361fe6bdb9ea8dfebec8
Karthi Mol <75553966+JuiceyBeans@users.noreply.github.com>
2026-06-28 08:36:29 +0100

    Specify as item tag event

--- overrides/kubejs/server_scripts/Tags.js
+++ overrides/kubejs/server_scripts/Tags.js
@@ -1,4 +1,4 @@
-ServerEvents.tags(event => {
+ServerEvents.tags('item', event => {
     event.add('cosmicfronters:runewood_ultimine_group', ['malum:runewood_log', 'malum:exposed_runewood_log'])
     event.add('cosmicfronters:soulwood_ultimine_group', ['malum:soulwood_log', 'malum:exposed_soulwood_log'])
 })
