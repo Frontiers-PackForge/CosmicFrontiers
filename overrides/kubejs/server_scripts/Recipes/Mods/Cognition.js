99c31feb252734fa0719ca408475313b03d0db51
Ghostipedia <Ghostipedia@gmail.com>
2026-07-20 07:51:24 -0400

    Cognition Obelisk Recipe

---
+++ overrides/kubejs/server_scripts/Recipes/Mods/Cognition.js
@@ -0,0 +1,3 @@
+ServerEvents.recipes(event => {
+event.shaped('cognition:experience_obelisk', [' A ', ' B ', 'ACA'], {A: 'minecraft:emerald', B: 'ars_nouveau:manipulation_essence', C: 'gtceu:steel_drum'})
+})
